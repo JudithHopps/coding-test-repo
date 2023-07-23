81dabe0b885b0437a9bed92dad307f537e9c3e0c
JudithHopps <113181934+JudithHopps@users.noreply.github.com>
2023-07-24 07:40:25 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 7.16 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=javascript) 
 
 ### 성능 요약
 
-메모리: 35.1 MB, 시간: 3.46 ms
+메모리: 38.2 MB, 시간: 7.16 ms
 
 ### 구분
 
--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,25 +1,9 @@
 function solution(s) {
-    let count = 0;
-    let deleteCnt = 0;
-    let str = s.slice()
-    while (str.length !== 1) {
-       let temp =  str.split('0').join('')
-       count += 1
-        deleteCnt +=  str.length - temp.length 
-    
-        str = (temp.length).toString(2)
+    var answer = [0,0];
+    while(s.length > 1) {
+        answer[0]++;
+        answer[1] += (s.match(/0/g)||[]).length;
+        s = s.match(/1/g).length.toString(2);
     }
-    return [count, deleteCnt]
+    return answer;
 }
-
-
-// 베스트 답변
-// function solution(s) {
-//     var answer = [0,0];
-//     while(s.length > 1) {
-//         answer[0]++;
-//         answer[1] += (s.match(/0/g)||[]).length;
-//         s = s.replace(/0/g, '').length.toString(2);
-//     }
-//     return answer;
-// }
