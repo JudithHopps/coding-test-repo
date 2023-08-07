2ea10e19fc3ab4cbab38af1c79581552da840e61
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-07 18:17:49 +0900

    [level 2] Title: 프로세스, Time: 0.32 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프로세스/프로세스.js
+++ 프로그래머스/lv2/42587. 프로세스/프로세스.js
@@ -1,24 +1,22 @@
 function solution(priorities, location) {
-    const answer = []
-    const origin = priorities.map((v,i) => {
-        return [v,i]
-    })
-    // console.log(origin)
-    
-    let queue = origin.slice()
-    
-    for(let i=0;i<priorities.length;i++) {
-    // Find the largest value at the first index in the queue array
-    let largestValue = queue.reduce((max, current,i) => {
-      return current[0] > max[0] ? [current[0],current[1],i] : max;
-    }, [queue[0][0],queue[0][1],0]);
-    
-    // console.log(largestValue)
-    answer.push(largestValue[1])
-    queue = queue.slice(largestValue[2]+1).concat(queue.slice(0,largestValue[2]))
-    // console.log(queue)
-    
+    var arr = priorities.map((priority, index) => {
+        return {
+            index: index, priority: priority
+        };
+    });
+
+    var queue = [];
+
+    while(arr.length > 0) {
+        var firstEle = arr.shift();
+        var hasHighPriority = arr.some(ele => ele.priority > firstEle.priority);
+        // console.log(firstEle, hasHighPriority )
+        if (hasHighPriority) {
+            arr.push(firstEle);
+        } else {
+            queue.push(firstEle);
+        }
     }
-    // console.log("A",answer)
-    return answer.indexOf(location)+1
+
+    return queue.findIndex(queueEle => queueEle.index === location) + 1;
 }
