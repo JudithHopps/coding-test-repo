cc928574d6f48946e038968ad3c05bd5d1ff5e6b
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-18 07:53:49 +0900

    [Bronze V] Title: A+B - 7, Time: 112 ms, Memory: 9364 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n')
+
+const answer = []
+
+for (let i=1;i<input.length;i++){
+    const [a,b] = input[i].split(' ').map(Number)
+    answer.push("Case #"+i+": "+(a+b))
+}
+
+console.log(answer.join('\n'))
