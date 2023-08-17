d12a99caff65840d9bf13b918d65981d5eb1babe
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-18 07:58:27 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 120 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.js
@@ -0,0 +1,8 @@
+const input = Number(require('fs').readFileSync('/dev/stdin').toString())
+
+const answer = []
+
+for (let i=1;i<=input;i++){
+    answer.push("*".repeat(i))
+}
+console.log(answer.join('\n'))
