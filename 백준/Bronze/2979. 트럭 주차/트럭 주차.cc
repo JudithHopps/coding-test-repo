0067d203ef13030d13fd1872b849cf232c9e0e5b
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-20 11:58:45 +0900

    [Bronze II] Title: 트럭 주차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.cc
@@ -1,27 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a, b, c;
-int s, e, sum;
-int result[100];
+int A,B,C,s,e,sum, a[104];
 
-int main()
-{
-        cin >> a >> b >> c;
-        for (int i = 0; i < 3; i++)
-        {
-                scanf("%d %d", &s, &e);
-
-                for (int j = s; j < e; j++)
-                {
-                        result[j]++;
-                }
-        }
-        for (int i = 0; i < 100; i++)
-        {
-            sum += a * (result[i] == 1) + b * 2 * (result[i] == 2) + c * 3 * (result[i] == 3);
-        }
-
-        cout << sum << "\n";
-
-        return 0;
+int main() {
+    cin >> A>>B>>C;
+    for(int i=0;i<3;i++){
+        cin >> s >> e;
+        for (int j=s;j<e;j++) a[j]++;
+    }
+    
+    for (int i : a){
+        sum += A * (i==1) + B*2*(i==2) + C*3*(i==3);
+    }
+    cout << sum << "";
+    return 0;
 }
