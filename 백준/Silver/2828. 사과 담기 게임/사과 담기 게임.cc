52f0819a391891adb1edef699b17459a89b176fb
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-25 12:04:13 +0900

    [Silver V] Title: 사과 담기 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
+++ 백준/Silver/2828. 사과 담기 게임/사과 담기 게임.cc
@@ -1,31 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, j, l = 1, r, temp, ret;
+int n, m, j, l = 1, r, cur, ret;
 
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+
   cin >> n >> m >> j;
+
   for (int i = 0; i < j; i++)
   {
     r = l + m - 1;
-    cin >> temp;
-    if (l <= temp && temp <= r)
-      continue;
-    else if (r < temp)
+    cin >> cur;
+    if (r < cur)
     {
-      ret += temp - r;
-      l = temp - m + 1;
+      ret += cur - r;
+      l = cur - m + 1;
     }
-    if (temp < l)
+    if (cur < l)
     {
-      ret += (l - temp);
-      l = temp;
-      // cout << "T\n";
+      ret += l - cur;
+      l = cur;
     }
-    // cout << "i :" << i << " " << ret << "\n";
   }
   cout << ret << "\n";
   return 0;
