b50ba7c3c4b4928db293116b32b2b76f4918d619
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-25 16:35:30 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,22 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, win, A, B, asum, bsum;
+int n, a, b, cur, asum, bsum;
 string hhmm, pre;
-
+// 1. hhmm -> mm으로 계산 -> a,b 득점 비교 후 이기고 있는 분 계산
 string print(int sum)
 {
   string hh = "00" + to_string(sum / 60);
   string mm = "00" + to_string(sum % 60);
   return hh.substr(hh.size() - 2, 2) + ":" + mm.substr(mm.size() - 2, 2);
 }
-int stringToInt(string s)
+int cnt(string a, string b)
 {
-  return atoi(s.c_str());
+  int acnt = atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
+  int bcnt = atoi(b.substr(0, 2).c_str()) * 60 + atoi(b.substr(3, 2).c_str());
+  return bcnt - acnt;
 }
-int go(string a, string b)
-{
-  return -(stringToInt(a.substr(0, 2)) * 60 + stringToInt(a.substr(a.size() - 2, 2))) + (stringToInt(b.substr(0, 2)) * 60 + stringToInt(b.substr(b.size() - 2, 2)));
-}
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -25,36 +24,23 @@ int main()
 
   cin >> n;
 
-  for (int i = 0; i < n; i++)
+  while (n--)
   {
-    cin >> win >> hhmm;
-    if (i > 0)
-    {
-      // cout << "pre : " << pre << " ";
-      if (A > B)
-      {
-        asum += go(pre, hhmm);
-      }
-      else if (A < B)
-      {
-        bsum += go(pre, hhmm);
-      }
-    }
-    win == 1 ? A++ : B++;
+    cin >> cur >> hhmm;
+    if (a > b)
+      asum += cnt(pre, hhmm);
+    if (b > a)
+      bsum += cnt(pre, hhmm);
+
+    cur == 1 ? a++ : b++;
+
     pre = hhmm;
-    // cout << "A : " << A << " B : " << B << "\n";
-    // cout << "A : " << asum << " B : " << bsum << "\n";
   }
-  if (A > B)
-  {
-    {
-      asum += go(hhmm, "48:00");
-    }
-  }
-  else if (A < B)
-  {
-    bsum += go(hhmm, "48:00");
-  }
+
+  if (a > b)
+    asum += cnt(hhmm, "48:00");
+  if (b > a)
+    bsum += cnt(hhmm, "48:00");
 
   cout << print(asum) << "\n"
        << print(bsum) << "\n";
