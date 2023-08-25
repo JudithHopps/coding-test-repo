4523da169f33ba0433b35a038ebeda6e2c050f6f
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-25 16:41:36 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret, i = 666;
+int n;
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -8,17 +8,17 @@ int main()
   cout.tie(NULL);
 
   cin >> n;
-  for (int i = 666;;)
+  for (int i = 666;; i++)
   {
     if (to_string(i).find("666") != string::npos)
     {
-      ret = i;
       n--;
       if (n == 0)
+      {
+        cout << i << "\n";
         break;
+      }
     }
-    i++;
   }
-  cout << ret << "\n";
   return 0;
 }
