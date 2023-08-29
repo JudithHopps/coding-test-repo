0ebdb3f7ab7752dc3f8c997a5d2af008edf789ee
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-29 12:44:46 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 4484 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,13 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define prev aaa
-#define next aaaa
-
 const int MAX = 200004;
-int visited[MAX], prev[MAX], n, k, ret, here, cnt, next;
-vector<int> v;
+#define prev aaaa
+int n, k, visited[MAX], prev[MAX];
 queue<int> q;
-
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -24,30 +20,31 @@ int main()
     q.pop();
     if (now == k)
     {
-      ret = visited[k];
       break;
     }
-
     for (int next : {now - 1, now + 1, now * 2})
     {
-      if (next >= MAX || next < 0 || visited[next])
+      if (next < 0 || next >= MAX || visited[next])
         continue;
       visited[next] = visited[now] + 1;
       prev[next] = now;
       q.push(next);
     }
   }
-
+  vector<int> v;
   for (int i = k; i != n; i = prev[i])
   {
     v.push_back(i);
   }
   v.push_back(n);
+  reverse(v.begin(), v.end());
 
   cout << visited[k] - 1 << "\n";
-  reverse(v.begin(), v.end());
-  for (int i : v)
-    cout << i << ' ';
+
+  for (int n : v)
+  {
+    cout << n << " ";
+  }
 
   return 0;
 }
