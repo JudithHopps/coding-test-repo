ea85ba7d9a025dade9010673ba339d656a90f860
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-31 21:05:19 +0900

    [Gold III] Title: 괄호 추가하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
+++ 백준/Gold/16637. 괄호 추가하기/괄호 추가하기.cc
@@ -3,7 +3,7 @@ using namespace std;
 int n, ret = -987654321;
 string str;
 vector<int> num;
-vector<char> operStr;
+vector<char> op;
 
 int oper(int a, char b, int c)
 {
@@ -14,23 +14,23 @@ int oper(int a, char b, int c)
   else
     return a * c;
 }
-void go(int idx, int sum)
+
+void dfs(int idx, int sum)
 {
   if (idx == num.size() - 1)
   {
-    ret = max(sum, ret);
+    ret = max(ret, sum);
     return;
   }
 
-  go(idx + 1, oper(sum, operStr[idx], num[idx + 1]));
+  dfs(idx + 1, oper(sum, op[idx], num[idx + 1]));
 
   if (idx + 2 <= num.size() - 1)
   {
-    int temp = oper(num[idx + 1], operStr[idx + 1], num[idx + 2]);
-    go(idx + 2, oper(sum, operStr[idx], temp));
+    int temp = oper(num[idx + 1], op[idx + 1], num[idx + 2]);
+    dfs(idx + 2, oper(sum, op[idx], temp));
   }
 }
-
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -38,17 +38,19 @@ int main()
   cout.tie(NULL);
 
   cin >> n >> str;
-
   for (int i = 0; i < n; i++)
   {
     if (i % 2)
-      operStr.push_back(str[i]);
+      op.push_back(str[i]);
     else
+    {
       num.push_back(str[i] - '0');
+    }
   }
 
-  go(0, num[0]);
+  dfs(0, num[0]);
 
   cout << ret << "\n";
+
   return 0;
 }
