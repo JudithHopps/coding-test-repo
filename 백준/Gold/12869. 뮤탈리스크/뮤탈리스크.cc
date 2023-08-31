13ce41b932ce6316a6689ef4fa0aa0b279bc7e39
subin <113181934+JudithHopps@users.noreply.github.com>
2023-08-31 20:22:51 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3180 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,26 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-int INF = 987654321;
-int a[3], n, visited[64][64][64];
-vector<vector<int>> v;
+int n, a[3], visited[64][64][64];
+vector<int> v = {1, 3, 9};
+vector<vector<int>> attack;
+
 struct A
 {
   int a, b, c;
 };
-queue<A> q;
-
-int attack[6][3] = {
-    {9, 3, 1},
-    {9, 1, 3},
-    {3, 1, 9},
-    {3, 9, 1},
-    {1, 3, 9},
-    {1, 9, 3}};
 
 int bfs(int a, int b, int c)
 {
-
   visited[a][b][c] = 1;
+  queue<A> q;
   q.push({a, b, c});
 
   while (q.size())
@@ -28,13 +20,9 @@ int bfs(int a, int b, int c)
     int a = q.front().a;
     int b = q.front().b;
     int c = q.front().c;
-
     q.pop();
 
-    if (visited[0][0][0])
-      break;
-
-    for (int i = 0; i < 6; i++)
+    for (int i = 0; i < attack.size(); i++)
     {
       int na = max(0, a - attack[i][0]);
       int nb = max(0, b - attack[i][1]);
@@ -62,7 +50,11 @@ int main()
     cin >> a[i];
   }
 
-  cout << bfs(a[0], a[1], a[2]) << "\n";
+  do
+  {
+    attack.push_back(v);
+  } while (next_permutation(v.begin(), v.end()));
 
+  cout << bfs(a[0], a[1], a[2]) << "\n";
   return 0;
 }
