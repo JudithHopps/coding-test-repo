d73f131d5ab80d92bdf6b752472e8f9d09646bef
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-05 18:39:07 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2052 KB -BaekjoonHub

--- 백준/Gold/2234. 성곽/성곽.cc
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -1,33 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, cnt, a[54][54], visited[54][54], area[2504], mx, big;
+int n, m, cnt, mx, big, a[54][54], visited[54][54], area[2504];
 const int dy[] = {0, -1, 0, 1};
 const int dx[] = {-1, 0, 1, 0};
-
 int dfs(int y, int x, int cnt)
 {
   if (visited[y][x])
     return 0;
   visited[y][x] = cnt;
-  int ret = 1;
+  int sum = 1;
   for (int i = 0; i < 4; i++)
   {
     if (!(a[y][x] & (1 << i)))
     {
       int ny = y + dy[i];
       int nx = x + dx[i];
-      ret += dfs(ny, nx, cnt);
+      sum += dfs(ny, nx, cnt);
     }
   }
-  return ret;
+  return sum;
 }
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  // 입력 세팅
+
   cin >> n >> m;
   for (int i = 0; i < m; i++)
   {
@@ -37,22 +35,20 @@ int main()
     }
   }
 
-  // 1번 방 개수
   for (int i = 0; i < m; i++)
   {
     for (int j = 0; j < n; j++)
     {
       if (!visited[i][j])
       {
+
         cnt++;
         area[cnt] = dfs(i, j, cnt);
-        // 2번 가장 큰 방
-        mx = max(area[cnt], mx);
+        mx = max(mx, area[cnt]);
       }
     }
   }
 
-  // 3번 방 방을 허문다면
   for (int i = 0; i < m; i++)
   {
     for (int j = 0; j < n; j++)
@@ -77,8 +73,9 @@ int main()
       }
     }
   }
-  cout << cnt << '\n'
-       << mx << '\n'
-       << big << '\n';
+
+  cout << cnt << "\n"
+       << mx << "\n"
+       << big << "\n";
   return 0;
 }
