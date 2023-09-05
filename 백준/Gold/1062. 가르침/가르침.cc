e97ca52e07de449ca6ae6367b3d9a2af93c84fcb
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-05 11:53:16 +0900

    [Gold IV] Title: 가르침, Time: 136 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.cc
+++ 백준/Gold/1062. 가르침/가르침.cc
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, words[54];
+int n, m, words[51];
 string s;
-
 int count(int mask)
 {
   int cnt = 0;
@@ -13,37 +12,30 @@ int count(int mask)
   }
   return cnt;
 }
-int go(int idx, int k, int mask)
+int go(int index, int cnt, int mask)
 {
-  if (k < 0)
+  if (cnt > m)
     return 0;
-  if (idx == 26)
+  if (index == 26)
     return count(mask);
-  int ret = go(idx + 1, k - 1, mask | (1 << idx));
-  if (idx != 'a' - 'a' && idx != 'n' != 'a' && idx != 'i' - 'a' && idx != 't' - 'a' && idx != 'c' - 'a')
+  int ret = go(index + 1, cnt + 1, mask | (1 << index));
+  if (index != 'a' - 'a' && index != 'n' - 'a' && index != 't' - 'a' && index != 'i' - 'a' && index != 'c' - 'a')
   {
-    ret = max(ret, go(idx + 1, k, mask));
+    ret = max(ret, go(index + 1, cnt, mask));
   }
   return ret;
 }
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
   cin >> n >> m;
-
   for (int i = 0; i < n; i++)
   {
     cin >> s;
-    for (char c : s)
+    for (char str : s)
     {
-      words[i] |= (1 << (c - 'a'));
+      words[i] |= (1 << (str - 'a'));
     }
   }
-
-  cout << go(0, m, 0) << "\n";
-
+  cout << go(0, 0, 0) << '\n';
   return 0;
 }
