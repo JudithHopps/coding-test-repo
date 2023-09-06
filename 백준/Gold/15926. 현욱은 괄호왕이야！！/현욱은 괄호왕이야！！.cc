6b339131fc2e2ee2c67e32b9ddf41684078edf81
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-06 15:08:09 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 4 ms, Memory: 3192 KB -BaekjoonHub

--- 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -1,15 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret, a[200004];
-
+int n, ret;
 string str;
 stack<int> st;
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+
+  st.push(-1);
 
   cin >> n >> str;
   for (int i = 0; i < n; i++)
@@ -20,29 +20,17 @@ int main()
     }
     else
     {
-      if (st.size() > 0)
+      st.pop();
+      if (!st.empty())
       {
-        int j = st.top();
-        st.pop();
-        a[i] = a[j] = 1;
+        ret = max(ret, i - st.top());
+      }
+      else
+      {
+        st.push(i);
       }
     }
   }
-
-  int cur = 0;
-  for (int i = 0; i < n; i++)
-  {
-    if (a[i] == 1)
-    {
-      cur++;
-    }
-    else
-    {
-      ret = max(ret, cur);
-      cur = 0;
-    }
-  }
-  ret = max(ret, cur);
 
   cout << ret << "\n";
 
