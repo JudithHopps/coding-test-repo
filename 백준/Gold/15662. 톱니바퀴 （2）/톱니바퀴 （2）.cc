a68bebc0c83e23b9ac9ca7564ae27938e05212ec
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-13 10:05:17 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,47 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k, a, b, ret;
+int t, k, a, b, ret;
 string s[1004];
 
-void rotateString(int pos, int dir)
+void rot(int pos, int dir)
 {
-  if (dir)
-    rotate(s[pos].begin(), s[pos].begin() + s[pos].size() - 1, s[pos].end());
-  else
+  if (!dir)
     rotate(s[pos].begin(), s[pos].begin() + 1, s[pos].end());
+  else
+    rotate(s[pos].begin(), s[pos].begin() + s[pos].size() - 1, s[pos].end());
 }
+
 int findL(int pos)
 {
   for (int i = pos; i > 0; i--)
   {
     if (s[i][6] == s[i - 1][2])
-    {
       return i;
-    }
   }
   return 0;
 }
 
 int findR(int pos)
 {
-  for (int i = pos; i <= n - 2; i++)
+  for (int i = pos; i <= t - 2; i++)
   {
     if (s[i][2] == s[i + 1][6])
     {
       return i;
     }
   }
-  return n - 1;
+  return t - 1;
 }
-
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> n;
-  for (int i = 0; i < n; i++)
+  cin >> t;
+  for (int i = 0; i < t; i++)
   {
     cin >> s[i];
   }
@@ -58,24 +56,22 @@ int main()
     int cnt = 1;
     for (int pos = a; pos >= l; pos--)
     {
-
-      rotateString(pos, (cnt % 2 ? b : !b));
+      rot(pos, (cnt % 2 ? b : !b));
       cnt++;
     }
+
     cnt = 0;
     for (int pos = a + 1; pos <= r; pos++)
     {
-      rotateString(pos, (cnt % 2 ? b : !b));
+      rot(pos, (cnt % 2 ? b : !b));
       cnt++;
     }
   }
-
-  for (int i = 0; i < n; i++)
+  for (int i = 0; i < t; i++)
   {
     if (s[i][0] == '1')
       ret++;
   }
   cout << ret << "\n";
-
   return 0;
 }
