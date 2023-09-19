bf1e19eee9587434e38135c01587e8a67466b056
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-19 18:35:15 +0900

    [Gold II] Title: 컵라면, Time: 68 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Gold/1781. 컵라면/컵라면.cc
+++ 백준/Gold/1781. 컵라면/컵라면.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, ret, a, b;
+int n, ret, d, c;
 vector<pair<int, int>> v;
 priority_queue<int, vector<int>, greater<int>> pq;
 
@@ -11,13 +11,11 @@ int main()
   cout.tie(NULL);
 
   cin >> n;
-
   for (int i = 0; i < n; i++)
   {
-    cin >> a >> b;
-    v.push_back({a, b});
+    cin >> d >> c;
+    v.push_back({d, c});
   }
-
   sort(v.begin(), v.end());
 
   for (int i = 0; i < n; i++)
@@ -33,6 +31,7 @@ int main()
     ret += pq.top();
     pq.pop();
   }
+
   cout << ret << "\n";
 
   return 0;
