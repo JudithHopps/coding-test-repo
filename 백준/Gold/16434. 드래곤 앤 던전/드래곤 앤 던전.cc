ed514f1b66822c453f875a71d91beaaa9c47ec6e
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-20 12:15:15 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 112 ms, Memory: 4916 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -1,26 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, attack, t[130004], a[130004], h[130004], ret;
+ll n, attack, t[123460], a[123460], h[123460], ret, lo = 1, hi = 1e18 + 4;
 
 bool ch(ll mid)
 {
-  ll mxH = mid;
+  ll maxH = mid;
   ll at = attack;
+
   for (int i = 0; i < n; i++)
   {
     if (t[i] == 1)
     {
-      ll cnt = h[i] / at + (h[i] % at ? 1 : 0);
-      mid -= (cnt - 1) * a[i];
+      ll cnt = h[i] / at + (h[i] % at ? 1 : 0) - 1;
+      mid -= cnt * a[i];
+      if (mid <= 0)
+        return false;
     }
-    if (t[i] == 2)
+    else
     {
       at += a[i];
-      mid = min(mxH, mid + h[i]);
+      mid = min(maxH, mid + h[i]);
     }
-    if (mid <= 0)
-      return false;
   }
   return true;
 }
@@ -29,7 +30,6 @@ int main()
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-
   cin >> n >> attack;
 
   for (int i = 0; i < n; i++)
@@ -37,11 +37,9 @@ int main()
     cin >> t[i] >> a[i] >> h[i];
   }
 
-  ll lo = 1, hi = 1e18 + 4;
   while (lo <= hi)
   {
     ll mid = (lo + hi) / 2;
-    // cout << "lo : " << lo << "  hi : " << hi << "\n";
     if (ch(mid))
     {
       hi = mid - 1;
@@ -54,6 +52,5 @@ int main()
   }
 
   cout << ret << "\n";
-
   return 0;
 }
