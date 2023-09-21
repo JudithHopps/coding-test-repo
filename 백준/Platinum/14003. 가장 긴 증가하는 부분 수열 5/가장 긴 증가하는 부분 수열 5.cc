8b5cff98d6444745d2543d31dcdd6b0a37002298
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-21 12:39:05 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 264 ms, Memory: 18020 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,30 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, lis[1000004], len, num;
-pair<int, int> ans[1000004];
-stack<int> st;
 const int INF = 1e9 + 4;
-
+int n, num, lis[1000004], len;
+pair<int, int> ans[1000004];
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  fill(lis, lis + 1000004, INF);
   cin >> n;
+  fill(lis, lis + n, INF);
   for (int i = 0; i < n; i++)
   {
     cin >> num;
     auto lower_pos = lower_bound(lis, lis + len, num);
     int pos = (int)(lower_pos - lis);
     if (*lower_pos == INF)
+    {
       len++;
+    }
     *lower_pos = num;
     ans[i].first = pos;
     ans[i].second = num;
   }
-
+  stack<int> st;
   for (int i = n - 1; i >= 0; i--)
   {
     if (ans[i].first == len - 1)
