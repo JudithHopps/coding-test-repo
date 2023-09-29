78fdcfe5d0bc3e89bfe5178a869f17281b390ccd
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-29 18:35:09 +0900

    [Gold V] Title: 톱니바퀴 (2), Time: 0 ms, Memory: 2056 KB -BaekjoonHub

--- 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
+++ 백준/Gold/15662. 톱니바퀴 （2）/톱니바퀴 （2）.cc
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
-int t, k, a, b, ret;
+typedef long long ll;
+int n, k, a, b, ret;
 string s[1004];
 
 void rot(int pos, int dir)
@@ -13,61 +14,63 @@ void rot(int pos, int dir)
 
 int findL(int pos)
 {
-  for (int i = pos; i > 0; i--)
+  for (int i = pos; i >= 1; i--)
   {
     if (s[i][6] == s[i - 1][2])
+    {
       return i;
+    }
   }
   return 0;
 }
 
 int findR(int pos)
 {
-  for (int i = pos; i <= t - 2; i++)
+  for (int i = pos; i <= n - 2; i++)
   {
     if (s[i][2] == s[i + 1][6])
     {
       return i;
     }
   }
-  return t - 1;
+  return n - 1;
 }
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> t;
-  for (int i = 0; i < t; i++)
+  cin >> n;
+  for (int i = 0; i < n; i++)
   {
     cin >> s[i];
   }
-
   cin >> k;
+
   for (int i = 0; i < k; i++)
   {
     cin >> a >> b;
     a--;
-    b = (b == 1 ? 1 : 0);
+    b = (b == -1 ? 0 : 1);
     int l = findL(a);
     int r = findR(a);
-
-    int cnt = 1;
+    int cnt = 0;
     for (int pos = a; pos >= l; pos--)
     {
-      rot(pos, (cnt % 2 ? b : !b));
+      rot(pos, cnt % 2 == 0 ? b : !b);
       cnt++;
     }
-
-    cnt = 0;
+    cnt = 1;
     for (int pos = a + 1; pos <= r; pos++)
     {
-      rot(pos, (cnt % 2 ? b : !b));
+      rot(pos, cnt % 2 == 0 ? b : !b);
       cnt++;
     }
   }
-  for (int i = 0; i < t; i++)
+
+  for (int i = 0; i < n; i++)
   {
     if (s[i][0] == '1')
       ret++;
