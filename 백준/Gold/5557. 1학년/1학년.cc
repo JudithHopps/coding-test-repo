68e9b5a96fdf985d505fd0801dac00f6ad23ea2b
subin <113181934+JudithHopps@users.noreply.github.com>
2023-09-30 19:37:03 +0900

    [Gold V] Title: 1학년, Time: 4 ms, Memory: 2040 KB -BaekjoonHub

---
+++ 백준/Gold/5557. 1학년/1학년.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, a[104], dp[104][21];
+
+ll go(int idx, int sum)
+{
+  if (sum < 0 || sum > 20)
+    return 0;
+  ll &ret = dp[idx][sum];
+  if (ret)
+    return ret;
+  if (idx == n - 2)
+  {
+    if (sum == a[n - 1])
+      return 1;
+    else
+      return 0;
+  }
+  ret += go(idx + 1, sum + a[idx + 1]);
+  ret += go(idx + 1, sum - a[idx + 1]);
+  return ret;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> a[i];
+  }
+
+  cout << go(0, a[0]) << "\n";
+
+  return 0;
+}
