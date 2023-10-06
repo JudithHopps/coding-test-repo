23d3324e021baaaf8353a67fd7763cef842a628a
subin <113181934+JudithHopps@users.noreply.github.com>
2023-10-06 21:58:34 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.27 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.cpp
@@ -7,26 +7,26 @@ const int dy[] = {0, 1, 0, -1};
 const int dx[] = {1, 0, -1, 0};
 
 vector<vector<int>> solution(int n) {
-    vector<vector<int>> answer(n, vector<int>(n, 0)); // Initialize answer as an n x n matrix filled with 0s
+vector<vector<int>> answer(n, vector<int>(n, 0)); 
 
-    int y = 0, x = 0, dir = 0;
+int y = 0, x = 0, dir = 0;
 
-    for (int i = 1; i <= n * n; i++) {
-        answer[y][x] = i;
+for (int i = 1; i <= n * n; i++) {
+    answer[y][x] = i;
 
-        int ny = y + dy[dir];
-        int nx = x + dx[dir];
+    int ny = y + dy[dir];
+    int nx = x + dx[dir];
 
-        if (ny < 0 || nx < 0 || ny >= n || nx >= n || answer[ny][nx] != 0) {
-            // Change direction when hitting boundaries or already filled cell
-            dir = (dir + 1) % 4;
-            ny = y + dy[dir];
-            nx = x + dx[dir];
-        }
+    if (ny < 0 || nx < 0 || ny >= n || nx >= n || answer[ny][nx] != 0) {
+        // Change direction when hitting boundaries or already filled cell
+        dir = (dir + 1) % 4;
+        ny = y + dy[dir];
+        nx = x + dx[dir];
+    } 
 
-        y = ny;
-        x = nx;
-    }
+    y = ny;
+    x = nx;
+}
 
-    return answer;
+return answer;
 }
