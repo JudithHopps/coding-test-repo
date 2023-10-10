0fdb5197e81f147c6d2773ef3eaf9f3708c544c2
subin <113181934+JudithHopps@users.noreply.github.com>
2023-10-10 19:36:05 +0900

    [Gold V] Title: 선 긋기, Time: 368 ms, Memory: 14440 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.cc
+++ 백준/Gold/2170. 선 긋기/선 긋기.cc
@@ -2,6 +2,7 @@
 using namespace std;
 int n, from, to, ret, l, r;
 vector<pair<int, int>> v;
+
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -9,29 +10,30 @@ int main()
   cout.tie(NULL);
 
   cin >> n;
-  while (n--)
+
+  for (int i = 0; i < n; i++)
   {
     cin >> from >> to;
     v.push_back({from, to});
   }
-  sort(v.begin(), v.end());
 
+  sort(v.begin(), v.end());
   l = v[0].first, r = v[0].second;
 
-  for (int i = 1; i < v.size(); i++)
+  for (int i = 1; i < n; i++)
   {
-    if (r >= v[i].first && r < v[i].second)
-    {
-      r = v[i].second;
-    }
-    else if (r < v[i].first)
+    if (r < v[i].first)
     {
       ret += (r - l);
       l = v[i].first, r = v[i].second;
     }
+    else if (v[i].first <= r && r < v[i].second)
+    {
+      r = v[i].second;
+    }
   }
-
   ret += (r - l);
   cout << ret << "\n";
+
   return 0;
 }
