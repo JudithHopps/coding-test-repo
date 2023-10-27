c90137270ad26eb9aa84d1b827dc419f2b83b75e
subin <113181934+JudithHopps@users.noreply.github.com>
2023-10-27 13:03:31 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,30 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[1004], cnt[1004], ret;
+int n, lis[1004], len, num;
+const int INF = 1004;
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
+
+  fill(lis, lis + 1004, INF);
+
   cin >> n;
   for (int i = 0; i < n; i++)
   {
-    cin >> a[i];
-  }
+    cin >> num;
+    auto lower_pos = lower_bound(lis, lis + len, num);
+    int pos = (int)(lower_pos - lis);
 
-  for (int i = 0; i < n; i++)
-  {
-    int mx = 0;
-    for (int j = 0; j < i; j++)
-    {
-      if (a[j] < a[i] && mx < cnt[j])
-      {
-        mx = cnt[j];
-      }
-    }
-    cnt[i] = mx + 1;
-    ret = max(cnt[i], ret);
+    if (lis[pos] == INF)
+      len++;
+    lis[pos] = num;
   }
-  cout << ret << "\n";
+  cout << len << "\n";
   return 0;
 }
