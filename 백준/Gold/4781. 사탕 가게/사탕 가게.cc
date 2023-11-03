4ed96ab29cabbd9b7e5a3399fea2bdeea9b68cae
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-03 10:37:09 +0900

    [Gold IV] Title: 사탕 가게, Time: 248 ms, Memory: 2224 KB -BaekjoonHub

--- 백준/Gold/4781. 사탕 가게/사탕 가게.cc
+++ 백준/Gold/4781. 사탕 가게/사탕 가게.cc
@@ -1,25 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m1, m2, have, a, c1, c2, cost, dp[100004];
+int n, money, f, s, dp[10004];
+
 int main()
 {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
   while (true)
   {
-    scanf("%d %d.%d", &n, &m1, &m2);
-    if (n == 0)
+    scanf("%d %d.%d", &n, &f, &s);
+    if (n == 0 && f == 0 && s == 0)
       break;
+
+    money = f * 100 + s;
     memset(dp, 0, sizeof(dp));
-    have = m1 * 100 + m2;
+
+    vector<int> c(n + 4), m(10004);
     for (int i = 0; i < n; i++)
     {
-      scanf("%d %d.%d", &a, &c1, &c2);
-      cost = c1 * 100 + c2;
-      for (int j = cost; j <= have; j++)
+      scanf("%d %d.%d", &c[i], &f, &s);
+      m[i] = f * 100 + s;
+
+      for (int j = m[i]; j <= money; j++)
       {
-        dp[j] = max(dp[j], dp[j - cost] + a);
+        dp[j] = max(dp[j], dp[j - m[i]] + c[i]);
       }
     }
-    printf("%d\n", dp[have]);
+
+    cout << dp[money] << "\n";
   }
 
   return 0;
