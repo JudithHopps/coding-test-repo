369d16cd6ec936f5d6ef3d24ebdb30cf89d31453
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-03 19:59:05 +0900

    [Silver III] Title: NBA 농구, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/2852. NBA 농구/NBA 농구.cc
+++ 백준/Silver/2852. NBA 농구/NBA 농구.cc
@@ -1,49 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a, b, cur, asum, bsum;
-string hhmm, pre;
-// 1. hhmm -> mm으로 계산 -> a,b 득점 비교 후 이기고 있는 분 계산
-string print(int sum)
-{
-  string hh = "00" + to_string(sum / 60);
-  string mm = "00" + to_string(sum % 60);
-  return hh.substr(hh.size() - 2, 2) + ":" + mm.substr(mm.size() - 2, 2);
-}
-int cnt(string a, string b)
-{
-  int acnt = atoi(a.substr(0, 2).c_str()) * 60 + atoi(a.substr(3, 2).c_str());
-  int bcnt = atoi(b.substr(0, 2).c_str()) * 60 + atoi(b.substr(3, 2).c_str());
-  return bcnt - acnt;
-}
+#define time tttt
+#pragma warning(disable : 4996)
 
+int n, w, pre, time, hh, mm, a, b, winA, winB;
+string print(int time)
+{
+  string ret = to_string(time / 60) + ":";
+  if (ret.size() == 2)
+    ret = "0" + ret;
+
+  string temp = to_string(time % 60);
+  while (temp.size() < 2)
+  {
+    temp = "0" + temp;
+  }
+  ret += temp;
+  return ret;
+}
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-
   cin >> n;
-
-  while (n--)
+  for (int i = 0; i < n; i++)
   {
-    cin >> cur >> hhmm;
+
+    scanf("%d %d:%d", &w, &hh, &mm);
+    time = hh * 60 + mm;
+
     if (a > b)
-      asum += cnt(pre, hhmm);
+    {
+      winA += time - pre;
+    }
     if (b > a)
-      bsum += cnt(pre, hhmm);
+      winB += time - pre;
 
-    cur == 1 ? a++ : b++;
+    w == 1 ? a++ : b++;
 
-    pre = hhmm;
+    pre = time;
   }
 
   if (a > b)
-    asum += cnt(hhmm, "48:00");
+  {
+    winA += 48 * 60 - pre;
+  }
   if (b > a)
-    bsum += cnt(hhmm, "48:00");
+    winB += 48 * 60 - pre;
 
-  cout << print(asum) << "\n"
-       << print(bsum) << "\n";
+  cout << print(winA) << "\n"
+       << print(winB) << "\n";
 
   return 0;
 }
