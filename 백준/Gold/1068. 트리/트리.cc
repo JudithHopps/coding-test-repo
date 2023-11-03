b56cb801e0d5b122b2090d21a8a62658dcaa5c62
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-03 23:09:27 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,24 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, t, root, ret, d;
+int n, a, m, root;
 vector<int> adj[54];
 
-int go(int a)
+int go(int here)
 {
   int cnt = 0, child = 0;
-  for (int n : adj[a])
+  for (int aa : adj[here])
   {
-    if (n == d)
+    if (aa == m)
       continue;
     child++;
-    cnt += go(n);
+    cnt += go(aa);
   }
   if (child == 0)
     return 1;
-
   return cnt;
 }
-
 int main()
 {
   ios_base::sync_with_stdio(false);
@@ -28,22 +26,21 @@ int main()
   cin >> n;
   for (int i = 0; i < n; i++)
   {
-    cin >> t;
-    if (t == -1)
+    cin >> a;
+    if (a == -1)
       root = i;
     else
-    {
-      adj[t].push_back(i);
-    }
+      adj[a].push_back(i);
   }
 
-  cin >> d;
-  if (d == root)
-    ret = 0;
+  cin >> m;
+
+  if (m == root)
+    cout << 0 << "\n";
   else
   {
-    ret = go(root);
+    cout << go(root) << "\n";
   }
-  cout << ret << "\n";
+
   return 0;
 }
