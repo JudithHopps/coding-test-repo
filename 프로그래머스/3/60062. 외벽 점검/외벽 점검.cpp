43d5f326842cf0c71c1f089a0a3d9699ec0b5f5a
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-16 15:56:53 +0900

    [level 3] Title: 외벽 점검, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -3,34 +3,36 @@
 using namespace std;
 const int INF = 987654321;
 
-vector<int> changeStarIdx(vector<int> weak, int start, int n){
-    if(start == 0) return weak;
-    vector<int> ret ;
-    for (int i=start;i<weak.size();i++) ret.push_back(weak[i]);
-    for(int i = 0;i<start;i++) ret.push_back(weak[i]+n);
-    return ret;
+vector<int> changeStart(vector<int> weak,int start,int n){
+    if(start ==0) return weak;
+    vector<int> v;
+    for(int i = start;i<weak.size();i++) v.push_back(weak[i]);
+    for(int i=0;i<start;i++) v.push_back(weak[i] + n);
+    return v;
 }
-int solution(int n, vector<int> weak, vector<int> dist) {
-    int ret = INF;
-    sort(dist.begin(),dist.end());
-    
-    do{
-        for(int j=0;j<(int)weak.size();j++){
-            vector<int> weakList = changeStarIdx(weak,j,n);
-            int idx = 0, flag = 0;
-            int curr = weakList[0] + dist[0];
-            
-            for(int i=1;i<weakList.size();i++){
-                if(weakList[i] > curr){
-                    if(idx +1 == dist.size()){
-                        flag = 1; break;
-                    }
-                    curr = weakList[i] + dist[++idx];
-                }
-            }
-            if(!flag) ret = min(ret, idx+1);
-        }
-    }while(next_permutation(dist.begin(), dist.end()));
-    
-    return (ret==INF ? -1 : ret);
+int solution(int n, vector<int> weak, vector<int> dist)
+{
+  int ret = INF;
+
+  sort(dist.begin(), dist.end());
+
+  do{
+      for(int j=0;j<weak.size();j++){
+          vector<int> newWeak = changeStart(weak,j,n);
+          int idx = 0, flag = 0;
+          int cur = newWeak[0] + dist[0];
+          
+          for(int i = 0;i<weak.size();i++){
+              if(cur < newWeak[i]){
+                  if(idx+1 == dist.size()) {
+                      flag = 1; break;
+                  }
+                  cur = newWeak[i] + dist[++idx];
+              }
+          }
+          if(!flag ) ret = min(ret,idx+1);
+      } 
+  }while(next_permutation(dist.begin(),dist.end()));
+
+  return (ret == INF ? -1 : ret);
 }
