ca98c90d561650cf198aea13200381b2e801cd48
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-21 16:06:24 +0900

    [Gold III] Title: 역사, Time: 100 ms, Memory: 2648 KB -BaekjoonHub

---
+++ 백준/Gold/1613. 역사/역사.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, k, s, e, a, b, c, dist[401][401], t;
+const int INF = 987654321;
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n >> k;
+  for (int i = 0; i < k; i++)
+  {
+    cin >> a >> b;
+    dist[a][b] = -1, dist[b][a] = 1;
+  }
+
+  for (int k = 1; k <= n; k++)
+  {
+    for (int i = 1; i <= n; i++)
+    {
+      for (int j = 1; j <= n; j++)
+      {
+        if (dist[i][k] == 1 && dist[k][j] == 1)
+          dist[i][j] = 1;
+        else if (dist[i][k] == -1 && dist[k][j] == -1)
+        {
+          dist[i][j] = -1;
+        }
+      }
+    }
+  }
+
+  cin >> t;
+  for (int i = 0; i < t; i++)
+  {
+    cin >> s >> e;
+    cout << dist[s][e] << "\n";
+  }
+  return 0;
+}
