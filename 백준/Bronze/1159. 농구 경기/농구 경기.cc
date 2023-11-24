b651f4dd21113192edf2053a14ffd4e71decfade
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-24 10:49:33 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,25 +1,28 @@
 #include <bits/stdc++.h>
-#define fail "PREDAJA"
-
 using namespace std;
+string str, ret;
 int n, cnt[26];
-string s,ret;
 
+int main()
+{
 
-int main() {
-    cin >> n;
-    for(int i=0;i<n;i++) {
-        cin >> s;
-        cnt[s[0] - 'a']++;
-    }
-            
-    for(int k=0;k<26;k++){
-        if (cnt[k] >= 5) ret += (k + 'a');
-    }
-    if (ret.size()) cout << ret << endl;
-    else cout << fail << endl;
-    
-        
-    
-    return 0;
+  cin >> n;
+
+  for (int i = 0; i < n; i++)
+  {
+    cin >> str;
+    cnt[str[0] - 'a']++;
+  }
+
+  for (int i = 0; i < 26; i++)
+  {
+    if (cnt[i] >= 5)
+      ret += (i + 'a');
+  }
+  if (ret.size())
+    cout << ret << "";
+  else
+    cout << "PREDAJA"
+         << "";
+  return 0;
 }
