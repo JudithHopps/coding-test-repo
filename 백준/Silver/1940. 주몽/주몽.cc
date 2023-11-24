b9a1b5547c07a870c65db9890f54a5736ad978d8
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-24 12:20:57 +0900

    [Silver IV] Title: 주몽, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.cc
+++ 백준/Silver/1940. 주몽/주몽.cc
@@ -1,27 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[15004], ret;
-
+int n, k, temp, l, r, ret;
+vector<int> v;
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> n >> m;
+  cin >> n >> k;
   for (int i = 0; i < n; i++)
   {
-    cin >> a[i];
+    cin >> temp;
+    v.push_back(temp);
   }
 
-  for (int i = 0; i < n; i++)
+  sort(v.begin(), v.end());
+  l = 0, r = n - 1;
+
+  while (l < r)
   {
-    for (int j = i + 1; j < n; j++)
+    int sum = v[l] + v[r];
+    if (sum == k)
     {
-      if (a[i] + a[j] == m)
-        ret++;
+      ret++;
+      l++, r--;
+    }
+    else if (sum > k)
+    {
+      r--;
+    }
+    else
+    {
+      l++;
     }
   }
+
   cout << ret << "\n";
+
   return 0;
 }
