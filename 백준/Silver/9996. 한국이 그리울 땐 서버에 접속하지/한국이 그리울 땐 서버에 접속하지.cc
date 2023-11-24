05313774991f75dd6b13df56637ad2e22cfeff32
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-24 11:28:27 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,34 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n;
-string pattern, s, ret;
-const string ok = "DA", no = "NE";
+string str, s, pre, suf;
+
 int main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> n >> pattern;
-  int star = pattern.find('*');
-  string left = pattern.substr(0, star);
-  string right = pattern.substr(star + 1);
-  // cout << right << "\n";
+  cin >> n >> str;
+
+  int idx = str.find('*');
+  pre = str.substr(0, idx);
+  suf = str.substr(idx + 1);
+
   for (int i = 0; i < n; i++)
   {
     cin >> s;
-    ret = ok;
-    if (s.size() < pattern.size() - 1)
-      ret = no;
+
+    if (s.size() < pre.size() + suf.size())
+    {
+      cout << "NE\n";
+    }
     else
     {
-      if (left != s.substr(0, star))
-        ret = no;
-      // cout << s.substr(s.size() - right.size()) << "\n";
-      if (right != s.substr(s.size() - right.size()))
-        ret = no;
+      if (pre == s.substr(0, idx) && suf == s.substr(s.size() - suf.size()))
+      {
+        cout << "DA\n";
+      }
+      else
+      {
+        cout << "NE\n";
+      }
     }
-    cout << ret << "\n";
   }
   return 0;
 }
