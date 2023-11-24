cb5670724937a1ebc3db11a67aea0338445592f1
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-24 11:45:11 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 184 ms, Memory: 25528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -1,32 +1,35 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int n,m;
+int n, m;
 string s;
+map<string, int> mp;
+map<int, string> mp2;
 
-map<string, int>mp;
-map<int,string>mp2;
-string a[100004];
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    
-    cin >> n >> m;
-    for(int i=0; i< n ; i++){
-        cin >> s;
-        mp[s] = i+1;
-        mp2[i+1] = s;
-        a[i+1] = s;
+  cin >> n >> m;
+  for (int i = 1; i <= n; i++)
+  {
+    cin >> s;
+    mp[s] = i;
+    mp2[i] = s;
+  }
+
+  for (int i = 0; i < m; i++)
+  {
+    cin >> s;
+    if (atoi(s.c_str()))
+    {
+      cout << mp2[atoi(s.c_str())] << "\n";
     }
-    
-    for (int i=0 ; i<m; i++){
-        cin >> s;
-        if (atoi(s.c_str())) {
-            cout << a[atoi(s.c_str())] << "\n";
-        } else {
-            cout << mp[s] << "\n";
-        }
+    else
+    {
+      cout << mp[s] << "\n";
     }
-    
-    return 0;
+  }
+  return 0;
 }
