70ca148a213a3bb4426fa233cbf94d196f61d637
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-24 11:38:51 +0900

    [Silver III] Title: 수열, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,36 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[100004], n, k, res = -10000000, psum[100004];
-
-// 최댓값 -> 최솟값부터 최대값.
-
-// 틀린 이유 : 최댓값이 0 이하일 수도 있다.
-// 최솟값을 가장 작은 값으로 갱신. 
-
+int a, n, k, psum[100004], ret = -987654321;
 
 int main()
 {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
   cin >> n >> k;
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
-    cin >> a[i];
-    psum[i] = psum[i - 1] + a[i];
-
-    if (i >= k)
-    {
-      // cout << "T"
-      //      << "\n";
-      if ((psum[i] - psum[i - k]) > res)
-      {
-        res = psum[i] - psum[i - k];
-        // cout << res << "\n";
-      }
-    }
-
-    // cout << "a[i]: " << a[i] << "psum[i]: " << psum[i] << "\n";
+    cin >> a;
+    psum[i + 1] = psum[i] + a;
   }
 
-  cout << res << "\n";
-
+  for (int i = k; i <= n; i++)
+  {
+    ret = max(ret, psum[i] - psum[i - k]);
+  }
+  cout << ret << "\n";
   return 0;
 }
