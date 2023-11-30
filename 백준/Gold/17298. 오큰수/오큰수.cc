b595308cc6e9ad4033e7d5f54e4ca81672515ed9
subin <113181934+JudithHopps@users.noreply.github.com>
2023-11-30 17:56:10 +0900

    [Gold IV] Title: 오큰수, Time: 244 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,28 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[1000004], ret[1000004];
-stack<int> s;
+int n, temp, a[1000004], ret[1000004];
+stack<int> st;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
-  fill(&ret[0], &ret[0] + 1000004, -1);
-  for (int i = 0; i < n; i++)
-  {
-    cin >> a[i];
-    while (s.size() && a[s.top()] < a[i])
+    cin >> n;
+    fill(&ret[0], &ret[0] + 1000004, -1);
+
+    for (int i = 0; i < n; i++)
     {
-      ret[s.top()] = a[i];
-      s.pop();
+        cin >> a[i];
     }
-    s.push(i);
-  }
-  for (int i = 0; i < n; i++)
-    cout << ret[i] << " ";
 
-  return 0;
+    for (int i = 0; i < n; i++)
+    {
+        while (st.size() && a[st.top()] < a[i])
+        {
+            ret[st.top()] = a[i];
+            st.pop();
+        }
+        st.push(i);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << ret[i] << "\n";
+    }
+
+    return 0;
 }
