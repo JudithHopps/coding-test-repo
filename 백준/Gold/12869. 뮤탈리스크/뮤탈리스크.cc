c5b132d5a67caf5b7ffe893d90da066c7613186e
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-01 19:46:39 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3176 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,58 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[3], visited[64][64][64];
-vector<vector<int>> attack;
-vector<int> v = {1, 3, 9};
-
+int a[6], n, ret = 987654321, visited[64][64][64];
 struct A
 {
-  int a, b, c;
+    int a, b, c;
 };
+int attack[6][3] = {
+    {9, 3, 1},
+    {9, 1, 3},
+    {3, 9, 1},
+    {3, 1, 9},
+    {1, 9, 3},
+    {1, 3, 9}};
 
-int bfs(int a, int b, int c)
+void bfs(int a, int b, int c)
 {
-  visited[a][b][c] = 1;
-  queue<A> q;
-  q.push({a, b, c});
+    visited[a][b][c] = 1;
+    queue<A> q;
+    q.push({a, b, c});
 
-  while (q.size())
-  {
-    int a = q.front().a;
-    int b = q.front().b;
-    int c = q.front().c;
-    q.pop();
-
-    for (auto at : attack)
+    while (q.size())
     {
-      int na = max(0, a - at[0]);
-      int nb = max(0, b - at[1]);
-      int nc = max(0, c - at[2]);
-      if (visited[na][nb][nc])
-        continue;
-      visited[na][nb][nc] = visited[a][b][c] + 1;
-      q.push({na, nb, nc});
-    }
-  }
-  return visited[0][0][0] - 1;
-}
+        if (visited[0][0][0])
+            break;
 
+        int a = q.front().a;
+        int b = q.front().b;
+        int c = q.front().c;
+        q.pop();
+
+        for (int i = 0; i < 6; i++)
+        {
+            int na = max(0, a - attack[i][0]);
+            int nb = max(0, b - attack[i][1]);
+            int nc = max(0, c - attack[i][2]);
+
+            if (visited[na][nb][nc])
+                continue;
+            visited[na][nb][nc] = visited[a][b][c] + 1;
+            q.push({na, nb, nc});
+        }
+    }
+    return;
+}
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
-  for (int i = 0; i < n; i++)
-  {
-    cin >> a[i];
-  }
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
-  do
-  {
-    attack.push_back(v);
-  } while (next_permutation(v.begin(), v.end()));
+    bfs(a[0], a[1], a[2]);
 
-  cout << bfs(a[0], a[1], a[2]) << "\n";
-  return 0;
+    cout << visited[0][0][0] - 1 << "\n";
+
+    return 0;
 }
