23940f2b2e19409a007980043c9472aa2a890f92
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-06 21:46:38 +0900

    [Gold III] Title: 사다리 조작, Time: 1424 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.cc
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.cc
@@ -1,71 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-
-int n, m, h, visited[34][34], ret = INF, a, b;
+int ret = INF, n, m, h, a, b, visited[34][34];
 
 bool check()
 {
-  for (int i = 1; i <= n; i++)
-  {
-    int ch = i;
-    for (int j = 1; j <= h; j++)
+    for (int k = 1; k <= n; k++)
     {
-      if (visited[j][ch])
-      {
-        ch++;
-      }
-      else if (visited[j][ch - 1])
-      {
-        ch--;
-      }
-    }
-    if (ch != i)
-    {
-      return false;
-    }
-  }
+        int now = k;
 
-  return true;
+        for (int i = 1; i <= h; i++)
+        {
+            if (visited[i][now - 1])
+                now--;
+            else if (visited[i][now])
+                now++;
+        }
+        if (now != k)
+            return false;
+    }
+    return true;
 }
-void go(int here, int cnt)
+void go(int start, int cnt)
 {
-  if (cnt > 3 || cnt > ret)
-    return;
-  if (check())
-  {
-    ret = min(ret, cnt);
-    return;
-  }
+    if (cnt > 3 || cnt > ret)
+        return;
 
-  for (int i = here; i <= h; i++)
-  {
-    for (int j = 1; j <= n; j++)
+    if (check())
     {
-      if (visited[i][j] || visited[i][j - 1] || visited[i][j + 1])
-        continue;
-      visited[i][j] = 1;
-      go(i, cnt + 1);
-      visited[i][j] = 0;
+        ret = min(ret, cnt);
+        return;
     }
-  }
+
+    for (int i = start; i <= h; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            if (visited[i][j] || visited[i][j - 1] || visited[i][j + 1])
+                continue;
+            visited[i][j] = 1;
+            go(i, cnt + 1);
+            visited[i][j] = 0;
+        }
+    }
+    return;
 }
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> m >> h;
-  for (int i = 0; i < m; i++)
-  {
-    cin >> a >> b;
-    visited[a][b] = 1;
-  }
+    cin >> n >> m >> h;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a >> b;
+        visited[a][b] = 1;
+    }
 
-  go(1, 0);
+    go(1, 0);
 
-  cout << (ret == INF ? -1 : ret) << "\n";
+    cout << (ret == INF ? -1 : ret) << "\n";
 
-  return 0;
+    return 0;
 }
