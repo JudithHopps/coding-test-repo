498ac25bbc0318973d389badf4ebd44aa7e52dee
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-06 20:08:37 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 3692 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.cc
+++ 백준/Silver/2529. 부등호/부등호.cc
@@ -1,62 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int k;
-vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-vector<vector<int>> ret;
-char a[15];
+int n;
+char a[10];
+vector<string> ret;
 
-bool oper(int a, char b, int c)
+void go(int idx, string s, int visited)
 {
-  if (b == '<')
-    return a < c;
-  else
-    return a > c;
-}
-void ch(vector<int> &v)
-{
-  bool flag = true;
-  for (int i = 0; i < k; i++)
-  {
-    if (!oper(v[i], a[i], v[i + 1]))
+    if (idx == n)
     {
-      flag = false;
-      break;
+        ret.push_back(s);
+        return;
     }
-  }
-  if (flag)
-  {
-    ret.push_back(v);
-  }
-}
+    char before = s[idx] - '0';
 
+    if (a[idx] == '<')
+    {
+        for (int i = before + 1; i < 10; i++)
+        {
+            if (visited & (1 << i))
+                continue;
+            go(idx + 1, s + to_string(i), visited | (1 << i));
+        }
+    }
+    else
+    {
+        for (int i = 0; i < before; i++)
+        {
+            if (visited & (1 << i))
+                continue;
+            go(idx + 1, s + to_string(i), visited | (1 << i));
+        }
+    }
+    return;
+}
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> k;
-  for (int i = 0; i < k; i++)
-  {
-    cin >> a[i];
-  }
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
 
-  do
-  {
-    ch(v);
+    for (int i = 0; i < 10; i++)
+    {
+        go(0, to_string(i), 1 << (i));
+    }
+    sort(ret.begin(), ret.end());
 
-  } while (next_permutation(v.begin(), v.end()));
+    cout
+        << ret[ret.size() - 1] << "\n"
+        << ret[0] << "\n";
 
-  sort(ret.begin(), ret.end());
-  for (int i = 0; i < k + 1; i++)
-  {
-    cout << ret[ret.size() - 1][i] << "";
-  }
-  cout << "\n";
-  for (int i = 0; i < k + 1; i++)
-  {
-    cout << ret[0][i] << "";
-  }
-
-  return 0;
+    return 0;
 }
