70432c5a67bae2a9c3c9afb2f558c45d5a878a67
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-07 10:38:40 +0900

    [Gold IV] Title: 다이어트, Time: 16 ms, Memory: 4800 KB -BaekjoonHub

--- 백준/Gold/19942. 다이어트/다이어트.cc
+++ 백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,60 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[4], ret = 987654321;
+int n, mp, mf, ms, mv, ret = 987654321;
 struct A
 {
-  int dan, gi, tan, vi, price;
-};
+    int p, f, s, v, c;
+} a[20];
 map<int, vector<vector<int>>> ans;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> a[0] >> a[1] >> a[2] >> a[3];
-  vector<A> v(n);
-  for (int i = 0; i < n; i++)
-  {
-    cin >> v[i].dan >> v[i].gi >> v[i].tan >> v[i].vi >> v[i].price;
-  }
+    cin >> n >> mp >> mf >> ms >> mv;
 
-  for (int i = 1; i < (1 << n); i++)
-  {
-    A temp = {0, 0, 0, 0};
-    vector<int> tempV;
-    for (int j = 0; j < n; j++)
+    for (int i = 0; i < n; i++)
     {
-      if (i & (1 << j))
-      {
-        tempV.push_back(j + 1);
-        temp.dan += v[j].dan, temp.gi += v[j].gi;
-        temp.tan += v[j].tan, temp.vi += v[j].vi, temp.price += v[j].price;
-      }
+        cin >> a[i].p >> a[i].f >> a[i].s >> a[i].v >> a[i].c;
     }
 
-    if (temp.dan >= a[0] && temp.gi >= a[1] && temp.tan >= a[2] && temp.vi >= a[3])
+    for (int i = 1; i < (1 << n); i++)
     {
-      ret = min(ret, temp.price);
-      ans[temp.price].push_back(tempV);
+        int p = 0, f = 0, s = 0, v = 0, c = 0;
+        vector<int> temp;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+            {
+                p += a[j].p;
+                f += a[j].f;
+                s += a[j].s;
+                v += a[j].v;
+                c += a[j].c;
+                temp.push_back(j + 1);
+            }
+        }
+
+        if (p >= mp && s >= ms && f >= mf && v >= mv)
+        {
+            ret = min(ret, c);
+            ans[c].push_back(temp);
+        }
     }
-  }
 
-  if (ret != 987654321)
-  {
-
-    sort(ans[ret].begin(), ans[ret].end());
-    cout << ret << "\n";
-    for (int a : ans[ret][0])
+    if (ret == 987654321)
     {
-      cout << a << " ";
+        cout << -1 << "\n";
     }
-  }
-  else
-  {
-    cout << -1 << "\n";
-  }
+    else
+    {
+        cout << ret << "\n";
+        sort(ans[ret].begin(), ans[ret].end());
 
-  return 0;
+        for (int i : ans[ret][0])
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
 }
