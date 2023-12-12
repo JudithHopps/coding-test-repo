7bb4f32a1c05c6d86fc3bc25fc5f9114be67d04c
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-12 18:58:31 +0900

    [Gold III] Title: 소수의 연속합, Time: 1368 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.cc
@@ -1,45 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, ret, l, h, sum;
 vector<int> v;
-int n, cnt, sum, hi, lo, ret, a[2000001];
+
+bool ch(int i)
+{
+    for (int j = 2; j * j <= i; j++)
+    {
+        if (i % j == 0)
+            return false;
+    }
+    return true;
+}
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
-
-  for (int i = 2; i <= n; i++)
-  {
-    bool flag = true;
-
-    for (int j = 2; j * j <= i; j++)
+    cin >> n;
+    for (int i = 2; i <= n; i++)
     {
-      if (i % j == 0)
-      {
-        flag = false;
-        break;
-      }
+        if (ch(i))
+            v.push_back(i);
     }
 
-    if (flag)
+    while (true)
     {
-      v.push_back(i);
+        if (sum >= n)
+        {
+            sum -= v[l++];
+        }
+        else if (h == v.size())
+        {
+            break;
+        }
+        else
+        {
+            sum += v[h++];
+        }
+        if (sum == n)
+            ret++;
     }
-  }
 
-  while (1)
-  {
-    if (sum >= n)
-      sum -= v[lo++];
-    else if (hi == v.size())
-      break;
-    else
-      sum += v[hi++];
-    if (sum == n)
-      ret++;
-  }
-  cout << ret << "\n";
+    cout << ret << "\n";
+    return 0;
 }
