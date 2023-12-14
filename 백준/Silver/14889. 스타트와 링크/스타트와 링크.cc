5115977dcbddfc3d18e63f7ed16238bc3df6d836
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-14 15:54:53 +0900

    [Silver I] Title: 스타트와 링크, Time: 116 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.cc
@@ -1,56 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-int n, s[24][24], ret = INF;
+int n, a[24][24], ret = INF;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
+    cin >> n;
+    for (int i = 0; i < n; i++)
     {
-      cin >> s[i][j];
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
     }
-  }
 
-  for (int i = 1; i < (1 << n); i++)
-  {
-    if (__builtin_popcount(i) != n / 2)
-      continue;
-    vector<int> A, B;
-    for (int j = 0; j < n; j++)
+    for (int i = 1; i < (1 << n) - 1; i++)
     {
-      if (i & (1 << j))
-      {
-        A.push_back(j);
-      }
-      else
-      {
-        B.push_back(j);
-      }
-    }
-    int asum = 0, bsum = 0;
-    for (int i = 0; i < n / 2; i++)
-    {
-      for (int j = 0; j < n / 2; j++)
-      {
-        if (i == j)
-          continue;
-        asum += s[A[i]][A[j]];
-        bsum += s[B[i]][B[j]];
-      }
-    }
-    ret = min(ret, abs(asum - bsum));
-    if (ret == 0)
-      break;
-  }
+        if (__builtin_popcount(i) != n / 2)
+            continue;
+        vector<int> team1, team2;
+        for (int j = 0; j < n; j++)
+        {
+            if (i & (1 << j))
+            {
+                team1.push_back(j);
+            }
+            else
+            {
+                team2.push_back(j);
+            }
+        }
 
-  cout << ret << "\n";
+        if (team1.size() == n / 2 && team2.size() == n / 2)
+        {
+            int sum1 = 0, sum2 = 0;
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = i + 1; j < n / 2; j++)
+                {
+                    sum1 += a[team1[i]][team1[j]];
+                    sum1 += a[team1[j]][team1[i]];
 
-  return 0;
+                    sum2 += a[team2[i]][team2[j]];
+                    sum2 += a[team2[j]][team2[i]];
+                }
+            }
+            ret = min(ret, abs(sum1 - sum2));
+            if (ret == 0)
+                break;
+        }
+    }
+
+    cout << ret << "\n";
+    return 0;
 }
