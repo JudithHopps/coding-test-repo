8404d77af91e69d04742072d9965a784e9aa4d93
subin <113181934+JudithHopps@users.noreply.github.com>
2023-12-15 19:01:41 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 56 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.cc
@@ -1,108 +1,115 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int INF = 987654321;
-const int dy[] = {0, 1, 0, -1};
-const int dx[] = {1, 0, -1, 0};
-int n, m, k, a[54][54], visited[54][54], b[54][54], r, c, s, ret = INF, sy, sx, ey, ex, dir;
+int n, m, k, a[54][54], b[54][54], r, c, s, sy, sx, ey, ex, dir, ret = INF;
+const int dy[] = {0, 1, 0, -1}, dx[] = {1, 0, -1, 0};
+int visited[54][54];
 struct A
 {
-  int r, c, s;
+    int r, c, s;
 };
-vector<A> v;
-vector<int> v_idx;
-vector<pair<int, int>> vv;
+vector<A> rot;
+vector<int> idx, v;
+vector<pair<int, int>> pairV;
 
 void go(int y, int x)
 {
-  if (y == sy && x == ex)
-    dir++;
-  if (y == ey && x == sx)
-    dir++;
-  if (y == ey && x == ex)
-    dir++;
+    if (y == sy && x == ex)
+        dir++;
+    if (y == ey && x == ex)
+        dir++;
+    if (y == ey && x == sx)
+        dir++;
 
-  int ny = y + dy[dir];
-  int nx = x + dx[dir];
-  if (visited[ny][nx])
+    int ny = y + dy[dir];
+    int nx = x + dx[dir];
+    if (visited[ny][nx])
+        return;
+    visited[ny][nx] = 1;
+    pairV.push_back({ny, nx});
+    v.push_back(b[ny][nx]);
+    go(ny, nx);
     return;
-
-  visited[ny][nx] = 1;
-  vv.push_back({ny, nx});
-  go(ny, nx);
 }
-void rotateAll(int y, int x, int s)
+void rotateAll(int r, int c, int s)
 {
-  for (int i = s; i > 0; i--)
-  {
-    memset(visited, 0, sizeof(visited));
-    vv.clear();
-    dir = 0;
-    sy = y - i;
-    sx = x - i;
-    ey = y + i;
-    ex = x + i;
-
-    visited[sy][sx] = 1;
-    vv.push_back({sy, sx});
-    go(sy, sx);
-
-    vector<int> vvv;
-    for (auto it : vv)
+    for (int i = 1; i <= s; i++)
     {
-      vvv.push_back(b[it.first][it.second]);
+        memset(visited, 0, sizeof(visited));
+        pairV.clear();
+        v.clear();
+        dir = 0;
+
+        sy = r - i;
+        sx = c - i;
+        ey = r + i;
+        ex = c + i;
+
+        visited[sy][sx] = 1;
+        pairV.push_back({sy, sx});
+        v.push_back(b[sy][sx]);
+
+        go(sy, sx);
+
+        // for (auto it : pairV)
+        // {
+        //     v.push_back(b[it.first][it.second]);
+        // }
+
+        rotate(v.begin(), v.end() - 1, v.end());
+
+        for (int i = 0; i < v.size(); i++)
+        {
+            b[pairV[i].first][pairV[i].second] = v[i];
+        }
     }
-    rotate(vvv.begin(), vvv.begin() + vvv.size() - 1, vvv.end());
-    for (int i = 0; i < vv.size(); i++)
-    {
-      b[vv[i].first][vv[i].second] = vvv[i];
-    }
-  }
+    return;
 }
 void solve()
 {
-  for (auto i : v_idx)
-  {
-    rotateAll(v[i].r, v[i].c, v[i].s);
-  }
-  for (int i = 0; i < n; i++)
-  {
-    int sum = 0;
-    for (int j = 0; j < m; j++)
+    for (int i : idx)
     {
-      sum += b[i][j];
+        rotateAll(rot[i].r, rot[i].c, rot[i].s);
     }
-    ret = min(sum, ret);
-  }
+    for (int i = 0; i < n; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < m; j++)
+        {
+            sum += b[i][j];
+        }
+        ret = min(sum, ret);
+    }
 }
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> m >> k;
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
+    cin >> n >> m >> k;
+    for (int i = 0; i < n; i++)
     {
-      cin >> a[i][j];
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+        }
     }
-  }
+    for (int i = 0; i < k; i++)
+    {
+        cin >> r >> c >> s;
+        r--, c--;
+        rot.push_back({r, c, s});
+        idx.push_back(i);
+    }
 
-  for (int i = 0; i < k; i++)
-  {
-    cin >> r >> c >> s;
-    r--, c--;
-    v.push_back({r, c, s});
-    v_idx.push_back(i);
-  }
+    do
+    {
+        memcpy(b, a, sizeof(b));
+        solve();
+    } while (next_permutation(idx.begin(), idx.end()));
 
-  do
-  {
-    memcpy(b, a, sizeof(a));
-    solve();
-  } while (next_permutation(v_idx.begin(), v_idx.end()));
+    cout << ret << "\n";
 
-  cout << ret << "\n";
-  return 0;
+    return 0;
 }
