7f091c23482ee2dc3693614dfd06b85fa2fe3070
subin <113181934+JudithHopps@users.noreply.github.com>
2024-01-31 14:34:34 +0900

    [Gold V] Title: 치킨 배달, Time: 4 ms, Memory: 2172 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -1,43 +1,66 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, m, a[54][54], result = 987654321;
-vector<vector<int>>chickenList;
-vector<pair<int, int>> _home, chicken;
-void combi(int start, vector<int> v){
-    if(v.size() == m){
-        chickenList.push_back(v);
-        return;
-    }
-    for(int i = start+1; i < chicken.size(); i++){
-        v.push_back(i);
-        combi(i, v);
-        v.pop_back();
-    }
+int n, m, result = 987654321, a[54][54];
+vector<pair<int, int>> chicken, home;
+vector<vector<int>> pick;
+vector<int> v;
+
+void combi(int start, vector<int> v)
+{
+  if (v.size() == m)
+  {
+    pick.push_back(v);
     return;
+  }
+
+  for (int i = start; i < chicken.size(); i++)
+  {
+    v.push_back(i);
+    combi(i + 1, v);
+    v.pop_back();
+  }
 }
-int main(){
-    cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> a[i][j];
-            if(a[i][j] == 1)_home.push_back({i, j});
-            if(a[i][j] == 2)chicken.push_back({i, j});
-        }
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n >> m;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      cin >> a[i][j];
+      if (a[i][j] == 1)
+      {
+        home.push_back({i, j});
+      }
+      else if (a[i][j] == 2)
+      {
+        chicken.push_back({i, j});
+      }
     }
-    vector<int> v;
-    combi(-1, v);
-    for(vector<int> cList : chickenList){
-        int ret = 0;
-        for(pair<int, int> home : _home){
-            int _min = 987654321;
-            for(int ch : cList){
-                int _dist = abs(home.first - chicken[ch].first) + abs(home.second - chicken[ch].second);
-                _min = min(_min, _dist);
-            }
-            ret += _min;
-        }
-        result = min(result, ret);
+  }
+
+  combi(0, v);
+
+  for (auto ch : pick)
+  {
+    int sum = 0;
+    for (auto h : home)
+    {
+      int mn = 987654321;
+      for (int c : ch)
+      {
+        int dist = abs(h.first - chicken[c].first) + abs(h.second - chicken[c].second);
+        mn = min(mn, dist);
+      }
+      sum += mn;
     }
-    cout << result << "\n";
-    return 0;
+    result = min(result, sum);
+  }
+
+  cout << result << "\n";
+  return 0;
 }
