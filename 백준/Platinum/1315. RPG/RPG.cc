e4507dd5200a1ad631467d5f5e7d3ea54a617dfc
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-07 10:44:41 +0900

    [Platinum III] Title: RPG, Time: 476 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Platinum/1315. RPG/RPG.cc
+++ 백준/Platinum/1315. RPG/RPG.cc
@@ -1,67 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, s, i, p, visited[54], dp[1004][1004];
-struct game
-{
-  int s, i, p;
-};
-vector<game> v;
+int S[54], I[54], P[54], n, visited[54], dp[1004][1004];
 
 int go(int STR, int INT)
 {
-  int &ret = dp[STR][INT];
-  if (ret != -1)
-    return ret;
-  ret = 0;
-  int p = 0;
-  vector<int> temp;
+    int &ret = dp[STR][INT];
+    if (ret != -1)
+        return ret;
+    ret = 0;
+    int p = 0;
+    vector<int> temp;
 
-  for (int i = 0; i < n; i++)
-  {
-    if (  v[i].i <= INT || v[i].s <= STR)
+    for (int i = 0; i < n; i++)
     {
-      ret++;
-      if (!visited[i])
-      {
-        visited[i] = 1;
-        temp.push_back(i);
-        p += v[i].p;
-      }
+        if (S[i] <= STR || I[i] <= INT)
+        {
+            ret++;
+            if (!visited[i])
+            {
+                p += P[i];
+                visited[i] = 1;
+                temp.push_back(i);
+            }
+        }
     }
-  }
 
-  for (int i = 0; i <= p; i++)
-  {
-    int nextS = min(1000, STR + i);
-    int nextI = min(1000, INT + (p - i));
+    for (int i = 0; i <= p; i++)
+    {
+        int nextSTR = min(1000, STR + i);
+        int nextINT = min(1000, INT + p - i);
+        ret = max(ret, go(nextSTR, nextINT));
+    }
 
-    ret = max(ret, go(nextS, nextI));
-  }
-
-  for (int tt : temp)
-  {
-    visited[tt] = 0;
-  }
-  return ret;
+    for (int tt : temp)
+    {
+        visited[tt] = 0;
+    }
+    return ret;
 }
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
 
-  memset(dp, -1, sizeof(dp));
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> S[i] >> I[i] >> P[i];
+    }
 
-  for (int i = 0; i < n; i++)
-  {
-    int j, k, l;
-    cin >> j >> k >> l;
-    v.push_back({j, k, l});
-  }
+    memset(dp, -1, sizeof(dp));
 
-  cout << go(1, 1) << "\n";
+    cout << go(1, 1) << "\n";
 
-  return 0;
+    return 0;
 }
