d503a47944d2f0e423699b419f3ede23e5204232
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-07 10:35:05 +0900

    [Platinum III] Title: RPG, Time: 480 ms, Memory: 5960 KB -BaekjoonHub

--- 백준/Platinum/1315. RPG/RPG.cc
+++ 백준/Platinum/1315. RPG/RPG.cc
@@ -1,47 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
+int n, s, i, p, visited[54], dp[1004][1004];
 struct game
 {
   int s, i, p;
 };
 vector<game> v;
-int n, s, l, p, dp[1001][1001];
-bool visited[54];
 
 int go(int STR, int INT)
 {
   int &ret = dp[STR][INT];
   if (ret != -1)
     return ret;
-
   ret = 0;
-  int pnt = 0;
-  vector<int> a;
+  int p = 0;
+  vector<int> temp;
 
   for (int i = 0; i < n; i++)
   {
-    if (v[i].s <= STR || v[i].i <= INT)
+    if (  v[i].i <= INT || v[i].s <= STR)
     {
       ret++;
       if (!visited[i])
       {
         visited[i] = 1;
-        pnt += v[i].p;
-        a.push_back(i);
+        temp.push_back(i);
+        p += v[i].p;
       }
     }
   }
 
-  for (int i = 0; i <= pnt; i++)
+  for (int i = 0; i <= p; i++)
   {
-    int nextSTR = min(1000, STR + i);
-    int nextINT = min(1000, INT + pnt - i);
-    ret = max(ret, go(nextSTR, nextINT));
+    int nextS = min(1000, STR + i);
+    int nextI = min(1000, INT + (p - i));
+
+    ret = max(ret, go(nextS, nextI));
   }
 
-  for (int i : a)
+  for (int tt : temp)
   {
-    visited[i] = 0;
+    visited[tt] = 0;
   }
   return ret;
 }
@@ -51,13 +50,16 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
+  memset(dp, -1, sizeof(dp));
+
   cin >> n;
+
   for (int i = 0; i < n; i++)
   {
-    cin >> s >> l >> p;
-    v.push_back({s, l, p});
+    int j, k, l;
+    cin >> j >> k >> l;
+    v.push_back({j, k, l});
   }
-  memset(dp, -1, sizeof(dp));
 
   cout << go(1, 1) << "\n";
 
