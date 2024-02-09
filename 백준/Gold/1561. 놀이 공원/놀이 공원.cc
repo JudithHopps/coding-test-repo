fc886e505a283f3172e450d5b9032fbbae64e6fb
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-09 11:48:42 +0900

    [Gold II] Title: 놀이 공원, Time: 4 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/1561. 놀이 공원/놀이 공원.cc
+++ 백준/Gold/1561. 놀이 공원/놀이 공원.cc
@@ -1,65 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-ll n, m, lo, hi = 1e12, ret, temp, a[10004];
+#define time tttt
 
-bool ch(ll mid)
+int a[10004], n, m;
+ll time, l = 1, r = 60 * 10000000004, ret;
+
+bool check(ll mid)
 {
-  ll sum = m;
-  for (ll i = 0; i < m; i++)
-  {
-    sum += mid / a[i];
-  }
-  return n <= sum;
+    ll sum = m;
+    for (int i = 0; i < m; i++)
+    {
+        sum += mid / a[i];
+    }
+    return sum >= n;
 }
-
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> m;
-  for (ll i = 0; i < m; i++)
-  {
-    cin >> a[i];
-  }
-  if (n <= m)
-  {
-    cout << n << "\n";
+    cin >> n >> m;
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a[i];
+    }
+
+    if (n <= m)
+    {
+        cout << n << "\n";
+        return 0;
+    }
+
+    while (l <= r)
+    {
+        ll mid = (l + r) / (1LL * 2);
+        if (check(mid))
+        {
+            time = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+
+    // cout << time << "\n";
+
+    int cnt = n - m;
+
+    time--;
+
+    for (int i = 0; i < m; i++)
+    {
+        cnt -= (time / a[i]);
+    }
+    time++;
+    for (int i = 0; i < m; i++)
+    {
+        cnt -= (time % a[i] == 0 ? 1 : 0);
+
+        if (cnt == 0)
+        {
+            ret = i + 1;
+            break;
+        }
+    }
+    // cout << cnt << "\n";
+    cout << ret << "\n";
+
     return 0;
-  }
-
-  while (lo <= hi)
-  {
-    ll mid = (lo + hi) / 2;
-    if (ch(mid))
-    {
-      ret = mid;
-      hi = mid - 1;
-    }
-    else
-    {
-      lo = mid + 1;
-    }
-  }
-
-  // cout << ret << "\n";
-
-  temp = m;
-  for (ll i = 0; i < m; i++)
-    temp += ((ret - 1) / a[i]);
-
-  for (ll i = 0; i < m; i++)
-  {
-    if (ret % a[i] == 0)
-      temp++;
-    if (temp == n)
-    {
-      cout << i + 1 << '\n';
-      return 0;
-    }
-  }
-
-  return 0;
 }
