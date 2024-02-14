34e6276424956104cb7edae813b8f9da397c05ca
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-14 15:40:52 +0900

    [Silver I] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.cc
+++ 백준/Silver/1074. Z/Z.cc
@@ -1,37 +1,37 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
+
+int n, r, c, cnt = -1, ret, siz;
+
+void go(int siz, int y, int x)
+{
+    if (siz == 1)
+    {
+        cnt++;
+        if (y == r && x == c)
+        {
+            ret = cnt;
+        }
+        return;
+    }
+    int next = siz / 2;
+
+    int xr = (x + next <= c && c < x + siz) ? 1 : 0;
+    int yr = (y + next <= r && r < y + siz) ? 1 : 0;
+    int mul = yr << 1 | xr;
+
+    cnt += mul * next * next;
+    go(next, y + yr * next, x + xr * next);
+}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n, r, c;
     cin >> n >> r >> c;
 
-    int size = 1 << n;
-    int result = 0;
+    siz = 1 << n;
 
-    for (int i = n - 1; i >= 0; i--)
-    {
-        int quadrant = 0;
-        int shift = 1 << i;
-        if (r >= shift)
-        {
-            quadrant += 2;
-            r -= shift;
-        }
-        if (c >= shift)
-        {
-            quadrant += 1;
-            c -= shift;
-        }
-        result += quadrant * shift * shift;
-    }
+    go(siz, 0, 0);
 
-    cout << result << "\n";
-
+    cout << ret << "\n";
     return 0;
 }
