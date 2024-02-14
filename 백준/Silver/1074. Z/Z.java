59dbdb70adc7e3583f9f2ac274ca8b07d44bce48
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-14 15:43:51 +0900

    [Silver I] Title: Z, Time: 200 ms, Memory: 17724 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,34 +1,38 @@
-
-
 import java.util.Scanner;
 
 public class Main {
-	public static int solve(int N, int r, int c, int cnt) {
-		if(N<=2) {
-			if(r==0 && c==0) return cnt;
-			if(r==0 && c==1) return cnt+1;
-			if(r==1 && c==0) return cnt+2;
-			if(r==1 && c==1) return cnt+3;
-		}
-		
-		int mid = N/2; //2
-		
-		if(r<mid && c<mid) return solve(mid, r, c, cnt);
-		if(r<mid && c>=mid) return solve(mid, r, c-mid, cnt+mid*mid);
-		if(r>=mid && c<mid) return solve(mid, r-mid, c, cnt+2*mid*mid);
-		//if(r>=mid && c>=mid)
-		return solve(mid, r-mid, c-mid, cnt+3*mid*mid);
-	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N, r, c;
-		N = sc.nextInt();
-		r = sc.nextInt();
-		c = sc.nextInt();
-		
-		System.out.println(solve(2<<N, r, c, 0));
-		sc.close();
-	}
+    static int n, r, c, cnt = -1, ret, siz;
 
+    public static void go(int siz, int y, int x) {
+        if (siz == 1) {
+            cnt++;
+            if (y == r && x == c) {
+                ret = cnt;
+            }
+            return;
+        }
+        int next = siz / 2;
+
+        int xr = (x + next <= c && c < x + siz) ? 1 : 0;
+        int yr = (y + next <= r && r < y + siz) ? 1 : 0;
+        int mul = yr << 1 | xr;
+
+        cnt += mul * next * next;
+        go(next, y + yr * next, x + xr * next);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        r = sc.nextInt();
+        c = sc.nextInt();
+
+        siz = 1 << n;
+
+        go(siz, 0, 0);
+
+        System.out.println(ret);
+        sc.close();
+    }
 }
