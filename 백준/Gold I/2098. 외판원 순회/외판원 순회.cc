e39361b0f9aacf17ff38d53105794f9e79d62a63
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-15 10:57:11 +0900

    [Gold I] Title: 외판원 순회, Time: 28 ms, Memory: 6116 KB -BaekjoonHub

---
+++ 백준/Gold I/2098. 외판원 순회/외판원 순회.cc
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, w[16][16], dp[16][1 << 16];
+const int INF = 98765432;
+
+int go(int here, int visited)
+{
+  if (visited == (1 << n) - 1)
+  {
+    return w[here][0] ? w[here][0] : INF;
+  }
+  int &ret = dp[here][visited];
+  if (ret != -1)
+    return ret;
+
+  ret = INF;
+  for (int i = 0; i < n; i++)
+  {
+    if (w[here][i] == 0 || visited & (1 << i))
+      continue;
+    ret = min(ret, go(i, visited | 1 << i) + w[here][i]);
+  }
+  return ret;
+}
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < n; j++)
+      cin >> w[i][j];
+  }
+
+  memset(dp, -1, sizeof(dp));
+
+  cout << go(0, 1) << "\n";
+
+  return 0;
+}
