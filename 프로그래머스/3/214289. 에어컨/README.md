5bcbb2ca38e4c2601699c4a962b71838c2a96cde
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-15 09:25:50 +0900

    [level 3] Title: 에어컨, Time: 0.22 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/3/214289. 에어컨/README.md
+++ 프로그래머스/3/214289. 에어컨/README.md
@@ -1,10 +1,10 @@
 # [level 3] 에어컨 - 214289 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/214289?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.37 MB, 시간: 0.62 ms
+메모리: 3.87 MB, 시간: 0.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 5일 12:22:39
+2024년 2월 4일 9:26:27
 
 ### 문제 설명
 
--- 프로그래머스/3/214289. 에어컨/에어컨.cpp
+++ 프로그래머스/3/214289. 에어컨/에어컨.cpp
@@ -1,34 +1,35 @@
-#include <vector>
 #include <string>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 const int INF = 987654321;
-int dp[1004][54];
 
 int solution(int temperature, int t1, int t2, int a, int b, vector<int> onboard) {
-  
+    //온도 10도 상승
+    int T = temperature +10 , T1 = t1+10 , T2 = t2+10;
     int n = onboard.size();
-    t1+=10, t2+=10,temperature+=10;
     
-    fill(&dp[0][0], &dp[0][0] +1004 *54,INF);
-    
-    dp[0][temperature] = 0;
+    // dp 초기화 
+    int dp[1004][54]; 
+    fill(&dp[0][0], &dp[0][0] + 1004*54,INF);
+    int now = T;
+    dp[0][now] = 0; 
     
     for(int i=0;i<n;i++){
-        for(int t=0;t<=50;t++){
-            if(onboard[i] &&(t < t1 || t2 < t)) continue;
-            if(dp[i][t] == INF) continue; 
-            dp[i+1][t+1] = min(dp[i+1][t+1], dp[i][t] + (temperature > t ? 0 : a));
-            dp[i+1][t]   = min(dp[i+1][t], dp[i][t] + (temperature == t? 0 : b));
-            dp[i+1][t-1] = min(dp[i+1][t-1] , dp[i][t] + (temperature < t ? 0 : a));
+        for(int t = 0 ;t <= 50;t++){
+            if(dp[i][t] == INF) continue;
+            if(onboard[i] && (T1 > t|| T2 < t ) )continue;
+            
+            dp[i+1][t-1] = min(dp[i+1][t-1], dp[i][t] + ((t> T) ? 0 : a));
+            dp[i+1][t]   = min(dp[i+1][t]  , dp[i][t] + ((t== T) ? 0 : b));
+            dp[i+1][t+1] = min(dp[i+1][t+1], dp[i][t] + ((t< T) ? 0 : a));
         }
     }
-    int ret = INF;
+    
+    // 결과값
+    int ret = INF; 
     for(int t=0;t<=50;t++){
-        ret = min(ret, dp[n][t]);
+        ret = min(ret,dp[n][t]);
     }
-    
-    
     return ret;
 }
