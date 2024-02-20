783619398401b9daa402db07d57a18e85341b60c
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-20 16:36:57 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 12 ms, Memory: 3700 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.cc
@@ -1,50 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 200004;
-#define prev aaaa
-int n, k, visited[MAX], prev[MAX];
+int n, k, visited[100004], pre[100004];
 queue<int> q;
+vector<int> v;
+
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> k;
-  visited[n] = 1;
-  q.push(n);
+    cin >> n >> k;
 
-  while (q.size())
-  {
-    int now = q.front();
-    q.pop();
-    if (now == k)
+    memset(pre, -1, sizeof(pre));
+
+    q.push(n);
+    visited[n] = 1;
+
+    while (q.size())
     {
-      break;
+        int now = q.front();
+        q.pop();
+
+        for (int next : {now - 1, now + 1, now * 2})
+        {
+            if (next < 0 || next > 100000)
+                continue;
+            if (visited[next])
+                continue;
+            visited[next] = visited[now] + 1;
+            pre[next] = now;
+            q.push(next);
+        }
     }
-    for (int next : {now - 1, now + 1, now * 2})
+
+    cout << visited[k] - 1 << "\n";
+
+    for (int i = k; i != -1; i = pre[i])
     {
-      if (next < 0 || next >= MAX || visited[next])
-        continue;
-      visited[next] = visited[now] + 1;
-      prev[next] = now;
-      q.push(next);
+        v.push_back(i);
     }
-  }
-  vector<int> v;
-  for (int i = k; i != n; i = prev[i])
-  {
-    v.push_back(i);
-  }
-  v.push_back(n);
-  reverse(v.begin(), v.end());
 
-  cout << visited[k] - 1 << "\n";
+    reverse(v.begin(), v.end());
 
-  for (int n : v)
-  {
-    cout << n << " ";
-  }
-
-  return 0;
+    for (int vv : v)
+    {
+        cout << vv << " ";
+    }
+    return 0;
 }
