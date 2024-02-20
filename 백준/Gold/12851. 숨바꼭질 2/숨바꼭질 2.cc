aef1313465eb7647833c51f03dce4fbc8b62853e
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-20 17:29:07 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, k, visited[100004], cnt[100004];
-queue<int> q;
 
 int main()
 {
@@ -10,22 +9,20 @@ int main()
     cout.tie(NULL);
 
     cin >> n >> k;
-    visited[n] = 1;
+    queue<int> q;
     q.push(n);
+    visited[n] = 1;
     cnt[n] = 1;
-
-    int now = n;
 
     while (q.size())
     {
         int now = q.front();
         q.pop();
 
-        for (int next : {now - 1, now + 1, now * 2})
+        for (int next : {now + 1, now - 1, now * 2})
         {
             if (next < 0 || next > 100000)
                 continue;
-
             if (visited[next] == visited[now] + 1)
             {
                 cnt[next] += cnt[now];
