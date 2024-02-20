3cdde99e16fc6227b3595ac302b3a3ffd3bf4d72
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-20 16:10:49 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 2932 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,51 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, k, visited[200004], cnt[200004];
+int n, k, visited[100004], cnt[100004];
+queue<int> q;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n >> k;
+    cin >> n >> k;
+    visited[n] = 1;
+    q.push(n);
+    cnt[n] = 1;
 
-  if (n == k)
-  {
-    cout << "0\n1\n";
-    return 0;
-  }
+    int now = n;
 
-  queue<int> q;
-  q.push(n);
-  visited[n] = 1;
-  cnt[n] = 1;
-
-  while (q.size())
-  {
-    int now = q.front();
-    q.pop();
-    for (int next : {now - 1, now + 1, now * 2})
+    while (q.size())
     {
-      if (next < 0 || next >= 200000)
-      {
-        continue;
-      }
-      if (!visited[next])
-      {
-        visited[next] = visited[now] + 1;
-        cnt[next] += cnt[now];
-        q.push(next);
-      }
-      else if (visited[next] == visited[now] + 1)
-      {
-        cnt[next] += cnt[now];
-      }
+        int now = q.front();
+        q.pop();
+
+        for (int next : {now - 1, now + 1, now * 2})
+        {
+            if (next < 0 || next > 100000)
+                continue;
+
+            if (visited[next] == visited[now] + 1)
+            {
+                cnt[next] += cnt[now];
+            }
+            else if (!visited[next])
+            {
+                visited[next] = visited[now] + 1;
+                q.push(next);
+                cnt[next] += cnt[now];
+            }
+        }
     }
-  }
 
-  cout << visited[k] - 1 << "\n"
-       << cnt[k] << "\n";
-
-  return 0;
+    cout << visited[k] - 1 << "\n"
+         << cnt[k] << "\n";
+    return 0;
 }
