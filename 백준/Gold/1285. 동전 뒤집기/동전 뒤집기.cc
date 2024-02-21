762f76ae2bc2df64659d1e08b7d9ef15baf2549a
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-21 18:00:48 +0900

    [Gold I] Title: 동전 뒤집기, Time: 312 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.cc
@@ -1,60 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[24], ret = 987654321;
+const int INF = 987654321;
+int n, a[24], ret = INF;
 string s;
 
-void cal()
+void check()
 {
     int sum = 0;
-    for (int i = 1; i < (1 << n); i *= 2)
+    for (int j = 0; j < n; j++)
     {
-        int cnt = 0;
-        for (int j = 0; j < n; j++)
+        int now = 0;
+        for (int i = 0; i < n; i++)
         {
-            if (a[j] & i)
-            {
-                cnt++;
-            }
+            if (a[i] & (1 << j))
+                continue;
+            now++;
         }
-        sum += min(cnt, n - cnt);
+        sum += min(n - now, now);
     }
+
     ret = min(ret, sum);
 }
-
-void go(int cur)
+void go(int idx)
 {
-    if (cur == n)
+    if (idx == n)
     {
-        cal();
+        check();
         return;
     }
-    go(cur + 1);
-    a[cur] = ~a[cur];
-    go(cur + 1);
+    go(idx + 1);
+    a[idx] = ~a[idx];
+    go(idx + 1);
+    return;
 }
+
 int main()
 {
+
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
 
     cin >> n;
+
     for (int i = 0; i < n; i++)
     {
         cin >> s;
-        int v = 1;
+        int k = (1 << (n - 1));
         for (int j = 0; j < n; j++)
         {
-            if (s[j] == 'T')
+            if (s[j] == 'H')
             {
-                a[i] |= v;
+                a[i] |= k;
             }
-            v *= 2;
+            k /= 2;
         }
     }
 
     go(0);
 
     cout << ret << "\n";
+
     return 0;
 }
