45961e12bca011c55d3cd1021e766339d940767d
subin <113181934+JudithHopps@users.noreply.github.com>
2024-02-23 16:01:39 +0900

    [Silver I] Title: 회의실 배정, Time: 24 ms, Memory: 3688 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.cc
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.cc
@@ -1,33 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, from, to, ret;
+int n, s, e, ret = 1;
 vector<pair<int, int>> v;
 
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
 
-  cin >> n;
-  for (int i = 0; i < n; i++)
-  {
-    cin >> from >> to;
-    v.push_back({to, from});
-  }
-  sort(v.begin(), v.end());
+    cin >> n;
 
-  from = v[0].second;
-  to = v[0].first;
-
-  for (auto it : v)
-  {
-    if (from <= it.second)
+    for (int i = 0; i < n; i++)
     {
-      ret++;
-      from = it.first;
+        cin >> s >> e;
+        v.push_back({e, s});
     }
-  }
-  cout << ret << "\n";
-  return 0;
+    sort(v.begin(), v.end());
+
+    int start = v[0].second;
+    int end = v[0].first;
+
+    for (int i = 1; i < n; i++)
+    {
+        if (end <= v[i].second)
+        {
+            ret++;
+            start = v[i].second;
+            end = v[i].first;
+        }
+    }
+
+    cout << ret << "\n";
+    return 0;
 }
