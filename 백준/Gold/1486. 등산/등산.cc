8cc763583e2582303f980bd9fbdbb117ae0a05cb
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-01 17:31:26 +0900

    [Gold II] Title: 등산, Time: 304 ms, Memory: 37180 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.cc
+++ 백준/Gold/1486. 등산/등산.cc
@@ -1,83 +1,69 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
-int n, m, t, d, a[26][26], b[3000][3000];
-char c;
 const int INF = 987654321;
-const int dy[] = {0, 0, 1, -1};
-const int dx[] = {1, -1, 0, 0};
+int n, m, T, D, h[30][30], cost[3000][3000];
+char c;
+const int dy[] = { 0,0,1,-1 };
+const int dx[] = { 1,-1,0,0 };
 vector<int> v;
 
-int main()
-{
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-  fill(&b[0][0], &b[0][0] + 3000 * 3000, INF);
+	cin >> n >> m >> T >> D;
+	fill(&cost[0][0], &cost[0][0] + 3000 * 3000, INF);
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> c;
+			if (c >= 'A' && c <= 'Z') {
+				h[i][j] = c - 'A';
+			}
+			else {
+				h[i][j] = c - 'a' + 26;
+			}
+			v.push_back( i * 100 + j );
+		}
+	}
 
-  cin >> n >> m >> t >> d;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			for (int d = 0; d < 4; d++) {
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      cin >> c;
-      if (c >= 'A' && c <= 'Z')
-      {
-        a[i][j] = c - 'A';
-      }
-      else
-      {
-        a[i][j] = c - 'a' + 26;
-      }
-      v.push_back(i * 100 + j);
-    }
-  }
+				int ny = i + dy[d];
+				int nx = j + dx[d];
+				if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+				int diff = abs(h[i][j] - h[ny][nx]);
+				if (diff <= T) {
+					if(h[i][j] >= h[ny][nx])
+						cost[i * 100 + j][ny * 100 + nx] = 1;
+					else
+						cost[i * 100 + j][ny * 100 + nx] = diff * diff;
+				}
+				
+			}
 
-  for (int y = 0; y < n; y++)
-  {
-    for (int x = 0; x < m; x++)
-    {
-      for (int i = 0; i < 4; i++)
-      {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
+		}
+	}
+	for (int k: v) {
+		for (int i : v) {
+			for (int j:v) {
+				if (cost[i][j] > cost[i][k] + cost[k][j]) {
+					cost[i][j] = cost[i][k] + cost[k][j];
+				}
+			}
+		}
+	}
 
-        if (ny < 0 || nx < 0 || ny >= n || nx >= m)
-          continue;
+	int ret = h[0][0];
+	for (int k : v) {
+		if(D >= cost[0][k] + cost[k][0])
+			ret = max(ret, h[k / 100][k % 100]);
+	}
+	cout << ret << "\n";
 
-        int diff = abs(a[ny][nx] - a[y][x]);
-        if (diff <= t)
-        {
-          if (a[y][x] < a[ny][nx])
-          {
-            b[y * 100 + x][ny * 100 + nx] = diff * diff;
-          }
-          else
-            b[y * 100 + x][ny * 100 + nx] = 1;
-        }
-      }
-    }
-  }
-
-  for (int k : v)
-  {
-    for (int i : v)
-    {
-      for (int j : v)
-      {
-        b[i][j] = min(b[i][j], b[i][k] + b[k][j]);
-      }
-    }
-  }
-
-  int ret = a[0][0];
-  for(int vv : v){
-      if(d >= b[0][vv] + b[vv][0]){
-          ret = max(ret,a[vv/100][vv%100]);
-      }
-  }
-  cout << ret << "\n";
-
-  return 0;
+	return 0;
 }
