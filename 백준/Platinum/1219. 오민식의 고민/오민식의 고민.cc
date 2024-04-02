c6fa697c2a5aef7827a5ff21349d51b12a1538da
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-02 09:53:04 +0900

    [Platinum V] Title: 오민식의 고민, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
+++ 백준/Platinum/1219. 오민식의 고민/오민식의 고민.cc
@@ -1,79 +1,76 @@
-#include <bits/stdc++.h>
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
 using namespace std;
-int n, s, e, m, a, b, c, ok, visited[104];
-long long cost[104], dist[104];
 const long long INF = -1e18;
-vector<pair<int, int>> adj[104];
+long long  dist[54], money[54];
+int n, s, e, m,a,b,c,visited[54];
+vector<pair<int,int>> adj[54];
 queue<int> q;
+bool ok=false;
+
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-  cin >> n >> s >> e >> m;
-  for (int i = 0; i < m; i++)
-  {
-    cin >> a >> b >> c;
-    adj[a].push_back({b, c});
-  }
+	cin >> n >> s >> e >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> a >> b >> c;
+		adj[a].push_back({ b,c });
+	}
+	for (int i = 0; i < n; i++) {
+		cin >> money[i];
+	}
+	fill(dist, dist + 54, INF);
+	dist[s] = money[s];
 
-  for (int i = 0; i < n; i++)
-  {
-    cin >> cost[i];
-  }
+	for (int i = 0; i < n; i++) {
+		for (int here = 0; here < n; here++) {
+			for (auto there : adj[here]) {
+				int next = there.first;
+				int d = there.second;
+				if (dist[here] != INF && dist[next] < dist[here] + money[next] - d) {
+					dist[next] = dist[here] + money[next] - d;
+					if (i == n - 1) {
+						q.push(here);
+					}
+				}
+			}
+		}
+	}
+	
 
-  fill(dist, dist + 104, INF);
-  dist[s] = cost[s];
+	while (q.size()) {
+		int here = q.front();
+		q.pop();
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int here = 0; here < n; here++)
-    {
-      for (auto there : adj[here])
-      {
-        int next = there.first;
-        int d = there.second;
+		for (auto there : adj[here]) {
+			int next = there.first;
+			if (next == e) {
+				ok = 1;
+				break;
+			}
+			if (!visited[next]) {
+				visited[next] = 1;
+				q.push(next);
+			}
+		}
+		if (ok) break;
+	}
 
-        if (dist[here] != INF && dist[next] < dist[here] + cost[next] - d)
-        {
-          dist[next] = dist[here] + cost[next] - d;
-          if (i == n - 1)
-            q.push(here);
-        }
-      }
-    }
-  }
+	if (ok) {
+		cout << "Gee\n";
+	}
+	else if (dist[e] == INF) {
+		cout << "gg\n";
+	}
+	else {
+		cout << dist[e] << "\n";
+	}
 
-  while (q.size())
-  {
-    int here = q.front();
-    q.pop();
-    for (auto there : adj[here])
-    {
-      int next = there.first;
-      if (next == e)
-      {
-        ok = 1;
-        break;
-      }
-      if (!visited[next])
-      {
-        visited[next] = 1;
-        q.push(next);
-      }
-    }
-    if (ok)
-      break;
-  }
-
-  if (ok)
-    cout << "Gee\n";
-  else if (dist[e] == INF)
-  {
-    cout << "gg\n";
-  }
-  else
-    cout << dist[e] << "\n";
-  return 0;
+	return 0;
 }
