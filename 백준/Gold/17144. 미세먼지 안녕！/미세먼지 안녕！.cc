0436719bbb99a0d3cadba67404d16781b257eeca
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-03 12:05:55 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 36 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.cc
@@ -1,143 +1,132 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
-int r, c, t, ret, a[104][104], temp[104][104], flag;
-const int dy1[] = {0, -1, 0, 1};
-const int dx1[] = {1, 0, -1, 0};
-const int dy2[] = {0, 1, 0, -1};
-const int dx2[] = {1, 0, -1, 0};
-vector<pair<int, int>> v1, v2;
+const int dy[] = { 0,-1,0,1 }, dx[] = { 1,0,-1,0 };
+const int dy2[] = { 0,1,0,-1 }, dx2[] = { 1,0,-1,0 };
 
-void miseGo()
-{
-    memset(temp, 0, sizeof(temp));
+int R, C, T, a[54][54], temp[54][54], ret,y,x;
+vector<pair<int,int>> v1, v2;
 
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (a[i][j] <= 0)
-                continue;
-            int mise = a[i][j] / 5;
-            for (int k = 0; k < 4; k++)
-            {
-                int ny = i + dy1[k];
-                int nx = j + dx1[k];
-                if (ny < 0 || nx < 0 || ny >= r || nx >= c)
-                    continue;
-                if (a[ny][nx] == -1)
-                    continue;
-                temp[i][j] -= mise;
-                temp[ny][nx] += mise;
-            }
-        }
-    }
+void dfs(int y, int x, int d,const int *dy, const int *dx, vector<pair<int, int>> &v) {
+	if (a[y][x] == -1)return;
+	
+	v.push_back({ y,x });
 
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (a[i][j] == -1)
-                continue;
-            a[i][j] = max(a[i][j] + temp[i][j], 0);
-            // cout << a[i][j] << " ";
-        }
-        // cout << "\n";
-    }
-    return;
+	int ny = y + dy[d];
+	int nx = x + dx[d];
+
+	if (ny < 0 || nx < 0 || ny >= R || nx >= C) {
+		d = (d + 1) % 4;
+		ny = y + dy[d];
+		nx = x + dx[d];
+	}
+
+	dfs(ny, nx, d, dy, dx, v);
+
 }
+void findRoute() {
+	dfs(y-1, x+1, 0,dy, dx,v1);
+	dfs(y, x+1, 0,dy2, dx2, v2);
 
-void clearArr(int i, int j, const int dy[], const int dx[], vector<pair<int, int>> &v)
-{
-    int y = i, x = j + 1;
-    int k = 0;
-
-    while (!(y == i && x == j))
-    {
-        v.push_back({y, x});
-        int ny = y + dy[k];
-        int nx = x + dx[k];
-
-        if (ny < 0 || nx < 0 || ny >= r || nx >= c)
-        {
-            k++;
-            ny = y + dy[k];
-            nx = x + dx[k];
-        }
-        y = ny, x = nx;
-    }
+	
 }
+void mise() {
+	memset(temp, 0, sizeof(temp));
+	for (int y = 0; y < R; y++) {
+		for (int x= 0; x < C; x++) {
+			if (a[y][x] <= 0)continue;
+			int q = a[y][x] / 5;
 
-void clearAir()
-{
-    vector<int> v;
-    for (auto it : v1)
-    {
-        v.push_back(a[it.first][it.second]);
-    }
-    a[v1[0].first][v1[0].second] = 0;
-    for (int i = 1; i < v1.size(); i++)
-    {
-        a[v1[i].first][v1[i].second] = v[i - 1];
-    }
-
-    v.clear();
-    for (auto it : v2)
-    {
-        v.push_back(a[it.first][it.second]);
-    }
-    a[v2[0].first][v2[0].second] = 0;
-    for (int i = 1; i < v2.size(); i++)
-    {
-        a[v2[i].first][v2[i].second] = v[i - 1];
-    }
+			for (int i = 0; i < 4; i++) {
+				int ny = y + dy[i];
+				int nx = x + dx[i];
+				if (ny < 0 || nx < 0 || ny >= R || nx >= C) continue;
+				if (a[ny][nx] == -1) continue;
+				temp[ny][nx] += q;
+				temp[y][x] -= q;
+			}
+		}
+	}
 }
+void reOrder() {
+	for (int y = 0; y < R; y++) {
+		for (int x = 0; x < C; x++) {
+			if (a[y][x] == -1)continue;
+			a[y][x] += temp[y][x];
+		}
+	}
+}
+void cleanAir(vector<pair<int, int>> &v) {
+	vector<int> temp;
 
+	for (int i = 0; i < v.size()-1; i++) {
+		temp.push_back(a[v[i].first][v[i].second]);
+	}
+
+	for (int i = 1; i < v.size(); i++) {
+		a[v[i].first][v[i].second] = temp[i - 1];
+	}
+	a[v[0].first][v[0].second] = 0;
+}
+void print(int arr[][54]) {
+	
+	for (int y = 0; y < R; y++) {
+		for (int x = 0; x < C; x++) {
+			cout << arr[y][x] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	//freopen("미세먼지.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> r >> c >> t;
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            cin >> a[i][j];
-            if (a[i][j] == -1 && flag)
-            {
-                clearArr(i, j, dy2, dx2, v2);
-            }
-            if (a[i][j] == -1 && !flag)
-            {
-                flag = 1;
-                clearArr(i, j, dy1, dx1, v1);
-            }
-        }
-    }
+	cin >> R >> C >> T;
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			cin >> a[i][j];
+			if (a[i][j] == -1) {
+				y = i, x = j;
+			}
+		}
+	}
 
-    // for (auto it : v2)
-    // {
-    //     cout << it.first << " , " << it.second << " \n";
-    // }
-    while (t--)
-    {
-        miseGo();
-        clearAir();
-    }
+	// 공기 청정기 경로 찾기
+	findRoute();
 
-    for (int i = 0; i < r; i++)
-    {
-        for (int j = 0; j < c; j++)
-        {
-            if (a[i][j] > 0)
-            {
-                ret += a[i][j];
-            }
-        }
-    }
+	/*for (auto it : v1) {
+		cout << it.first << " , " << it.second << "\n";
+	}*/
 
-    cout << ret << "\n";
+	while (T--) {
+		mise();
+		//print(temp);
+		reOrder(); 
 
-    return 0;
+		//print(a);
+		cleanAir(v1);
+		cleanAir(v2);
+		//print(a);
+	}
+
+	for (int i = 0; i < R; i++) {
+		for (int j = 0; j < C; j++) {
+			if (a[i][j] > 0) {
+				ret += a[i][j];
+			}
+			
+		}
+	}
+
+	cout << ret << "\n";
+	return 0;
 }
