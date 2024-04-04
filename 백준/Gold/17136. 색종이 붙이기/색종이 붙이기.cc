38fe4a963c5823cf3ea03b00fc80f1078f1f6623
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-04 14:05:56 +0900

    [Gold II] Title: 색종이 붙이기, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.cc
@@ -1,88 +1,81 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
-const int INF = 987654321;
-map<int, int> mp;
-int a[10][10], ret = INF, n = 10;
+const int n = 10,INF = 987654321;
+int a[14][14], ret = INF, visited[14][14],sy,sx, cnt[10];
+// 1이 적힌 칸은 모두 색종이로 덮여져야 한다
+// 1×1, 2×2, 3×3, 4×4, 5×5 종류의 색종이는 5개씩 가지고 있다.
+bool flag = false;
 
-bool ch(int y, int x, int siz)
-{
-  if (y + siz > n || x + siz > n)
-    return false;
-  for (int i = y; i < y + siz; i++)
-  {
-    for (int j = x; j < x + siz; j++)
-    {
-      if (a[i][j] == 0)
-        return false;
-    }
-  }
-  return true;
+bool ch(int y,int x ,int siz) {
+	if (y + siz > n || x + siz > n) return false;
+
+	for (int ny = y; ny < y + siz; ny++) {
+		for (int nx = x; nx < x + siz; nx++) {
+			if (a[ny][nx] != 1) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+void paint(int y, int x, int siz, int val) {
+	for (int ny = y; ny < y + siz; ny++) {
+		for (int nx = x; nx < x + siz; nx++) {
+			a[ny][nx] = val;
+		}
+	}
 }
 
-void draw(int y, int x, int siz, int val)
-{
-  for (int i = y; i < y + siz; i++)
-  {
-    for (int j = x; j < x + siz; j++)
-    {
-      a[i][j] = val;
-    }
-  }
-  return;
+void go(int y, int x, int use) {
+	if (ret <= use) return;
+	if (y >= n) {
+		ret = min(ret, use);
+		return;
+	}
+	if (x >= n) {
+		go(y + 1, 0, use);
+		return;
+	}
+	if (a[y][x] == 0) {
+		go(y, x + 1, use);
+		return;
+	}
+
+	for (int siz = 5; siz >= 1; siz--) {
+		if (cnt[siz] == 5) continue;
+		if (!ch(y, x, siz)) continue;
+		cnt[siz]++;
+		paint(y, x, siz, 0); // 색종이 덮기
+		go(y, x + 1, use + 1);
+		paint(y, x, siz, 1); // 색종이 제거
+		cnt[siz]--;
+	}
 }
-void dfs(int y, int x, int cnt)
-{
-  if (cnt >= ret)
-    return;
-  if (x == n)
-  {
-    dfs(y + 1, 0, cnt);
-    return;
-  }
-  if (y == n)
-  {
-    ret = min(ret, cnt);
-    return;
-  }
-  if (a[y][x] == 0)
-  {
-    dfs(y, x + 1, cnt);
-    return;
-  }
 
-  for (int i = 5; i >= 1; i--)
-  {
-    if (mp[i] == 5)
-      continue;
-    if (ch(y, x, i))
-    {
-      mp[i]++;
-      draw(y, x, i, 0);
-
-      dfs(y, x + i, cnt + 1);
-
-      draw(y, x, i, 1);
-      mp[i]--;
-    }
-  }
-}
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+	//freopen("색종이.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < n; j++)
-    {
-      cin >> a[i][j];
-    }
-  }
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> a[i][j];
+			if (a[i][j] && !flag) {
+				flag = true;
+				sy = i; sx = j;
+			}
+		}
+	}
 
-  dfs(0, 0, 0);
+	go(0,0, 0);
 
-  cout << (ret == INF ? -1 : ret) << "\n";
 
-  return 0;
+	cout << (ret==INF? -1 : ret) << "\n";
+	return 0;
 }
