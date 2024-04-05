51795bf8117e6bd354cac334f82607903770a987
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-05 16:43:03 +0900

    [Gold IV] Title: 팰린드롬?, Time: 268 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.cc
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[2004], dp[2004][2004], t, from, to;
+int n, a[2004], from, to, t, dp[2004][2004];
 
 int main()
 {
@@ -9,20 +9,20 @@ int main()
   cout.tie(NULL);
 
   cin >> n;
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n; i++)
   {
     cin >> a[i];
     dp[i][i] = 1;
   }
-
-  for (int i = 1; i <= n; i++)
+  for (int i = 0; i < n - 1; i++)
   {
     if (a[i] == a[i + 1])
       dp[i][i + 1] = 1;
   }
+
   for (int size = 2; size <= n; size++)
   {
-    for (int i = 1; i <= n - size; i++)
+    for (int i = 0; i < n - size; i++)
     {
       if (a[i] == a[i + size] && dp[i + 1][i + size - 1])
       {
@@ -35,8 +35,8 @@ int main()
   while (t--)
   {
     cin >> from >> to;
+    from--, to--;
     cout << dp[from][to] << "\n";
   }
-
   return 0;
 }
