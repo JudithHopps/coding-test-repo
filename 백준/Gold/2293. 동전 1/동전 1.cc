da2e0e2db04e7103d82749101755285e2eacba1e
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-07 15:22:20 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.cc
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-// 경우의 수 : 더하기!!
-int dp[10001], temp, n, k;
+int n, k, coin, dp[10004];
 
 int main()
 {
@@ -9,17 +8,18 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  cin >> n >> k;
   dp[0] = 1;
+  cin >> n >> k;
   for (int i = 0; i < n; i++)
   {
-    cin >> temp;
-    for (int j = temp; j <= k; j++)
+    cin >> coin;
+
+    for (int j = coin; j <= k; j++)
     {
-      dp[j] += dp[j - temp];
+      dp[j] += dp[j - coin];
     }
   }
-
   cout << dp[k] << "\n";
+
   return 0;
 }
