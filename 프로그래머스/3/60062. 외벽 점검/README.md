7a830f32017d6370f236b9baf9724298b6fd075d
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-08 10:39:42 +0900

    [level 3] Title: 외벽 점검, Time: 0.01 ms, Memory: 3.71 MB -BaekjoonHub

--- 프로그래머스/3/60062. 외벽 점검/README.md
+++ 프로그래머스/3/60062. 외벽 점검/README.md
@@ -1,10 +1,10 @@
 # [level 3] 외벽 점검 - 60062 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60062?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60062) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.22 ms
+메모리: 3.71 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 11월 4일 16:28:43
+2024년 04월 08일 10:40:18
 
 ### 문제 설명
 
--- 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
+++ 프로그래머스/3/60062. 외벽 점검/외벽 점검.cpp
@@ -3,36 +3,36 @@
 using namespace std;
 const int INF = 987654321;
 
-vector<int> changeStart(vector<int> weak,int start,int n){
+vector<int> changeStartIdx(vector<int> weak, int start,int n){
     if(start ==0) return weak;
-    vector<int> v;
-    for(int i = start;i<weak.size();i++) v.push_back(weak[i]);
-    for(int i=0;i<start;i++) v.push_back(weak[i] + n);
-    return v;
+    
+    vector<int> temp;
+    for(int i=start;i<weak.size();i++) temp.push_back(weak[i]);
+    for(int i=0;i<start;i++) temp.push_back(weak[i] + n);
+    
+    return temp;
 }
-int solution(int n, vector<int> weak, vector<int> dist)
-{
-  int ret = INF;
-
-  sort(dist.begin(), dist.end());
-
-  do{
-      for(int j=0;j<weak.size();j++){
-          vector<int> newWeak = changeStart(weak,j,n);
-          int idx = 0, flag = 0;
-          int cur = newWeak[0] + dist[0];
-          
-          for(int i = 0;i<weak.size();i++){
-              if(cur < newWeak[i]){
-                  if(idx+1 == dist.size()) {
-                      flag = 1; break;
-                  }
-                  cur = newWeak[i] + dist[++idx];
-              }
-          }
-          if(!flag ) ret = min(ret,idx+1);
-      } 
-  }while(next_permutation(dist.begin(),dist.end()));
-
-  return (ret == INF ? -1 : ret);
+int solution(int n, vector<int> weak, vector<int> dist) {
+    int ret = INF;
+    sort(dist.begin(),dist.end());
+    
+    do{
+        for(int j=0;j<weak.size();j++){
+            vector<int> Weak = changeStartIdx(weak,j,n);
+            int idx = 0, flag = 0;
+            int curr = Weak[0] + dist[idx];
+            for(int i=1;i<Weak.size();i++){
+                if(Weak[i] > curr){
+                    if(idx +1 == dist.size()){
+                        flag = 1; break;
+                    }
+                    curr = Weak[i] + dist[++idx];
+                }
+            }
+            if(!flag) ret = min(ret,idx+1);
+        }
+        
+        
+    } while(next_permutation(dist.begin(),dist.end()));
+    return (ret==INF ? -1 : ret);
 }
