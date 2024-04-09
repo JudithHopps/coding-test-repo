451276d7746db187236f4c8e1c99e2b97037e743
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-09 16:18:24 +0900

    [Silver III] Title: 01타일, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.cc
+++ 백준/Silver/1904. 01타일/01타일.cc
@@ -1,6 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
 
-#include <stdio.h>
 #include <iostream>
 using namespace std;
 const int MOD = 15746;
@@ -8,22 +6,13 @@ int n,dp[1000004];
 
 int main()
 {
-	//freopen("data.txt", "r", stdin);
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-
 	cin >> n;
-	// 1
-	// 00, 11
-	// 001,111, 100
-	// 0011, 0000, 1001, 1100, 1111
 	dp[1] = 1;
 	dp[2] = 2;
 
 	for (int i = 3; i <= n; i++) {
-		dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+		dp[i] = (dp[i - 1] % MOD + dp[i - 2] % MOD) ;
 	}
-	cout << dp[n] << "\n";
+	cout << dp[n] % MOD << "\n";
 	return 0;
 }
