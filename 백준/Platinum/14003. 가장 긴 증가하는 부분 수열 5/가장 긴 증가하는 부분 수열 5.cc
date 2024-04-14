d8129472489bc6ddcad62ebe53e9aac34cd5a476
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-14 17:36:09 +0900

    [Platinum V] Title: 가장 긴 증가하는 부분 수열 5, Time: 264 ms, Memory: 18020 KB -BaekjoonHub

--- 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
+++ 백준/Platinum/14003. 가장 긴 증가하는 부분 수열 5/가장 긴 증가하는 부분 수열 5.cc
@@ -1,11 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define mx_N 1000004
+const int mx_n = 1000004;
 const int INF = 1e9 + 4;
 
-int n, num, lis[mx_N], len;
+int n, lis[mx_n], num, len;
 stack<int> st;
-pair<int, int> ans[mx_N];
+pair<int, int> ans[mx_n];
 
 int main()
 {
@@ -13,7 +13,7 @@ int main()
   cin.tie(NULL);
   cout.tie(NULL);
 
-  fill(lis, lis + mx_N, INF);
+  fill(lis, lis + mx_n, INF);
   cin >> n;
 
   for (int i = 0; i < n; i++)
@@ -21,22 +21,22 @@ int main()
     cin >> num;
     auto lower_pos = lower_bound(lis, lis + len, num);
     int pos = (int)(lower_pos - lis);
+
     if (lis[pos] == INF)
       len++;
     lis[pos] = num;
-    ans[i].first = pos;
-    ans[i].second = num;
+    ans[i] = {pos, num};
   }
 
   for (int i = n - 1; i >= 0; i--)
   {
-    // cout << ans[i].first << " , " << ans[i].second << "\n";
     if (ans[i].first == len - 1)
     {
       st.push(ans[i].second);
       len--;
     }
   }
+
   cout << st.size() << "\n";
   while (st.size())
   {
