9494951ade54b8f9c8d0dd3dfb42f2163a71c18f
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-16 10:37:32 +0900

    [Bronze I] Title: 소인수분해, Time: 116 ms, Memory: 11788 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstring>
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int n;
+bool a[10000004];
+
+void era() {
+	memset(a, 1, sizeof(a));
+	a[0] = a[1] = false;
+
+	for (int i = 2; i <= n; i++) {
+		if (a[i]==false) continue;
+		for (int j = i * 2; j <= n; j += i) {
+			a[j] = false;
+		}
+	}
+}
+int main()
+{
+	//freopen("data.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+
+
+	cin >> n;
+	if (n == 1) return 0;
+
+	era();
+	for (int i = 2; i <= n; i++) {
+		
+		while (n%i == 0) {
+			cout << i << "\n";
+			n /= i;
+		}
+	}
+	return 0;
+}
