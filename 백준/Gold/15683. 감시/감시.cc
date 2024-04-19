46183d4f1e02c168aeaf74ff7c51706f1d71eeb6
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-19 13:57:38 +0900

    [Gold IV] Title: 감시, Time: 16 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.cc
+++ 백준/Gold/15683. 감시/감시.cc
@@ -1,181 +1,121 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
 using namespace std;
-typedef vector<pair<int, int>> vpii;
-int n, m, a[10][10], temp[10][10], ret = 987654321;
-vector<pair<int, int>> v;
-const int dy[] = {-1, 0, 1, 0};
-const int dx[] = {0, 1, 0, -1};
+typedef vector<pair<int, int>> vpi;
+const int dy[] = { 0,1,0,-1 };
+const int dx[] = { 1,0,-1,0 };
+vpi cctv;
 
-vpii extendCCTV(int here, int dir)
-{
-  vpii _change;
-  int y = v[here].first;
-  int x = v[here].second;
-  if (a[y][x] == 1)
-  {
-    while (true)
-    {
-      int ny = y + dy[dir];
-      int nx = x + dx[dir];
-      if (ny >= 0 && ny < n && nx >= 0 && nx < m && a[ny][nx] != 6)
-      {
-        if (a[ny][nx] == 0)
-        {
-          a[ny][nx] = 8;
-          _change.push_back({ny, nx});
-        }
-        y = ny;
-        x = nx;
-      }
-      else
-        break;
-    }
-  }
-  else if (a[y][x] == 2)
-  {
-    for (int i = 0; i <= 2; i += 2)
-    {
-      int _y = y;
-      int _x = x;
-      while (true)
-      {
-        int ny = _y + dy[(dir + i) % 4];
-        int nx = _x + dx[(dir + i) % 4];
-        if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
-        {
-          if (a[ny][nx] == 0)
-          {
-            a[ny][nx] = 8;
-            _change.push_back({ny, nx});
-          }
-          _y = ny;
-          _x = nx;
-        }
-        else
-          break;
-      }
-    }
-  }
-  else if (a[y][x] == 3)
-  {
-    for (int i = 0; i < 2; i++)
-    {
-      int _y = y;
-      int _x = x;
-      while (true)
-      {
-        int ny = _y + dy[(dir + i) % 4];
-        int nx = _x + dx[(dir + i) % 4];
-        if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
-        {
-          if (a[ny][nx] == 0)
-          {
-            a[ny][nx] = 8;
-            _change.push_back({ny, nx});
-          }
-          _y = ny;
-          _x = nx;
-        }
-        else
-          break;
-      }
-    }
-  }
-  else if (a[y][x] == 4)
-  {
-    for (int i = 0; i < 3; i++)
-    {
-      int _y = y;
-      int _x = x;
-      while (true)
-      {
-        int ny = _y + dy[(dir + i) % 4];
-        int nx = _x + dx[(dir + i) % 4];
-        if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
-        {
-          if (a[ny][nx] == 0)
-          {
-            a[ny][nx] = 8;
-            _change.push_back({ny, nx});
-          }
-          _y = ny;
-          _x = nx;
-        }
-        else
-          break;
-      }
-    }
-  }
-  else if (a[y][x] == 5)
-  {
-    for (int i = 0; i < 4; i++)
-    {
-      int _y = y;
-      int _x = x;
-      while (true)
-      {
-        int ny = _y + dy[(dir + i) % 4];
-        int nx = _x + dx[(dir + i) % 4];
-        if (ny >= 0 && nx >= 0 && ny < n && nx < m && a[ny][nx] != 6)
-        {
-          if (a[ny][nx] == 0)
-          {
-            a[ny][nx] = 8;
-            _change.push_back({ny, nx});
-          }
-          _y = ny;
-          _x = nx;
-        }
-        else
-          break;
-      }
-    }
-  }
-  return _change;
+const int INF = 987654321;
+int n, m, a[10][10], ret = INF;
+
+int cal() {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (a[i][j] == 0) cnt++; // 감시되지 않는 영역만 카운트
+		}
+	}
+	return cnt;
 }
+bool in(int y, int x) {
+	if (y < 0 || x < 0 || y >= n || x >= m) return false;
+	return true;
+}
+vpi go(int idx, int d) {
+	int y = cctv[idx].first;
+	int x = cctv[idx].second;
+	
+	int cctvType = a[y][x];
+	vpi now;
+	if (cctvType == 1) {
+		int ny = y;
+		int nx = x;
+		while (true) {
+			ny += dy[(d + 1) % 4];
+			nx += dx[(d + 1) % 4];
+			if (in(ny, nx) && a[ny][nx] != -1) {
+				if (a[ny][nx] == 0) {
+					a[ny][nx] = -10; // 감시 영역 표시
+					now.push_back({ ny,nx });
+				}
+			}
+			else break;
+		}
+	}
 
-void dfs(int here)
-{
-  if (here == v.size())
-  {
-    int cnt = 0;
-    for (int i = 0; i < n; i++)
-    {
-      for (int j = 0; j < m; j++)
-      {
-        if (a[i][j] == 0)
-          cnt++;
-      }
-    }
-    ret = min(cnt, ret);
-    return;
-  }
+	else if (cctvType == 2) {
+		for (int i = 0; i <= 2; i+=2) { 
+			int ny = y, nx = x;
+			while (true) {
+				ny += dy[(d + i) % 4];
+				nx += dx[(d + i) % 4];
+				if (in(ny, nx) && a[ny][nx] != -1) {
+					if (a[ny][nx] == 0) {
+						a[ny][nx] = -10; // 감시 영역 표시
+						now.push_back({ ny,nx });
+					}
+				}
+				else break;
+			}
+		}
+	}
+	else {
+		for (int i = 0; i < cctvType-1; i ++) {
+			int ny = y, nx = x;
+			while (true) {
+				ny += dy[(d + i) % 4];
+				nx += dx[(d + i) % 4];
+				if (in(ny, nx) && a[ny][nx] != -1) {
+					if (a[ny][nx] == 0) {
+						a[ny][nx] = -10; // 감시 영역 표시
+						now.push_back({ ny,nx });
+					}
+				}
+				else break;
+			}
+		}
+	}
 
-  for (int k = 0; k < 4; k++)
-  {
-    vpii _change = extendCCTV(here, k);
-    dfs(here + 1);
-    for (auto b : _change)
-      a[b.first][b.second] = 0;
-  }
+	return now;
+}	
+
+void dfs(int idx) {
+	if (idx == cctv.size()) {
+		ret = min(ret, cal());
+		return;
+	}
+	for (int k = 0; k < 4; k++) {
+		vpi now = go(idx, k);
+		dfs(idx + 1);
+		for (auto it : now) {
+			a[it.first][it.second] = 0;
+		}
+	}
 }
 int main()
 {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
+	//freopen("data.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-  cin >> n >> m;
-  for (int i = 0; i < n; i++)
-  {
-    for (int j = 0; j < m; j++)
-    {
-      cin >> a[i][j];
-      if (a[i][j] != 6 && a[i][j] != 0)
-        v.push_back({i, j});
-    }
-  }
-  dfs(0);
-  cout << ret << "\n";
-  return 0;
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];
+			if (a[i][j] == 6) a[i][j] = -1;
+			if (a[i][j] > 0) {
+				cctv.push_back({ i,j });
+			}
+		}
+	}
+	//cout << "dfs 전 \n";
+	dfs(0);
+
+	cout << ret << "\n";
+	return 0;
 }
