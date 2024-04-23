a879abaf7f1becdbebeb21389e34e5f158d4a895
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-23 16:06:40 +0900

    [Gold IV] Title: 뮤탈리스크, Time: 0 ms, Memory: 3176 KB -BaekjoonHub

--- 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
+++ 백준/Gold/12869. 뮤탈리스크/뮤탈리스크.cc
@@ -1,63 +1,59 @@
-#include <bits/stdc++.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
-int a[6], n, ret = 987654321, visited[64][64][64];
-struct A
-{
-    int a, b, c;
+
+struct A {
+	int a, b, c;
 };
-int attack[6][3] = {
-    {9, 3, 1},
-    {9, 1, 3},
-    {3, 9, 1},
-    {3, 1, 9},
-    {1, 9, 3},
-    {1, 3, 9}};
+int n, a,b,c,visited[64][64][64];
+int mutal[][6] = {
+	{9,3,1}, {9,1,3},
+	{3,9,1}, {3,1,9},
+	{1,3,9}, {1,9,3}
+};
 
-void bfs(int a, int b, int c)
-{
-    visited[a][b][c] = 1;
-    queue<A> q;
-    q.push({a, b, c});
 
-    while (q.size())
-    {
-        if (visited[0][0][0])
-            break;
-
-        int a = q.front().a;
-        int b = q.front().b;
-        int c = q.front().c;
-        q.pop();
-
-        for (int i = 0; i < 6; i++)
-        {
-            int na = max(0, a - attack[i][0]);
-            int nb = max(0, b - attack[i][1]);
-            int nc = max(0, c - attack[i][2]);
-
-            if (visited[na][nb][nc])
-                continue;
-            visited[na][nb][nc] = visited[a][b][c] + 1;
-            q.push({na, nb, nc});
-        }
-    }
-    return;
-}
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	//freopen("data.txt", "r", stdin);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> n;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+	cin >> n;
+	
+	cin >> a;
+	if (n >= 2) cin >> b;
+	if(n==3)	cin>> c;
 
-    bfs(a[0], a[1], a[2]);
+	visited[a][b][c] = 1;
 
-    cout << visited[0][0][0] - 1 << "\n";
+	queue<A> q;
+	q.push({ a,b,c });
 
-    return 0;
+	while (q.size()) {
+		int a = q.front().a;
+		int b = q.front().b;
+		int c = q.front().c;
+		q.pop();
+
+		for (int i = 0; i < 6; i++) {
+			int na = max(0, a - mutal[i][0]);
+			int nb = max(0, b - mutal[i][1]);
+			int nc = max(0, c - mutal[i][2]);
+
+			if (visited[na][nb][nc]) continue;
+			visited[na][nb][nc] = visited[a][b][c] + 1;
+			q.push({ na,nb,nc });
+		}
+	}
+
+
+	cout << visited[0][0][0] -1<< "\n";
+
+	return 0;
 }
