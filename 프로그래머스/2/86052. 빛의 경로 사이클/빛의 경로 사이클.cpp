f58832c8b5a3359599713c06802cc3144920d132
subin <113181934+JudithHopps@users.noreply.github.com>
2024-04-24 09:52:27 +0900

    [level 2] Title: 빛의 경로 사이클, Time: 99.80 ms, Memory: 56.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/86052. 빛의 경로 사이클/빛의 경로 사이클.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <cstring> 
+#include <algorithm>
+using namespace std;
+int dy[] = {0,1,0,-1};
+int dx[] = {1,0,-1,0};
+vector<string> Grid;
+int n,m;
+bool visited[510][510][4];
+
+int change_dir(char c, int d){
+    if(c == 'L') {
+        d = (d+3) % 4; 
+    } else if (c=='R') {
+        d = (d+1) % 4; 
+    }
+    return d; 
+}
+
+int go (int y, int x ,int d, int len){
+    if(visited[y][x][d]) return len; 
+    visited[y][x][d] = true;
+    
+    int nd = change_dir(Grid[y][x], d); 
+    int ny = (y +dy[nd]+n)%n;
+    int nx = (x + dx[nd]+m)%m;
+    return go(ny,nx,nd,len+1);
+    
+}
+vector<int> solution(vector<string> grid) {
+    vector<int> answer;
+    Grid = grid;
+    n = grid.size(); 
+    m = grid[0].size(); 
+    
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            for(int d=0;d<4;d++){
+                int ret = go(i,j,d,0);
+                if(ret) answer.push_back(ret);
+            }
+        }
+    }
+    sort(answer.begin(),answer.end());
+    return answer;
+}
