c60039abc0455d427c06bf41cf33e0c6a09c270a
subin <113181934+JudithHopps@users.noreply.github.com>
2024-05-02 10:49:51 +0900

    [level 3] Title: 단속카메라, Time: 4.65 ms, Memory: 4.34 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.cpp
@@ -6,11 +6,12 @@ using namespace std;
 
 bool cmp (vector<int> a, vector<int> b){
     if(a[0] != b[0]) return a[0] > b[0]; 
-    return a[1] > b[1];
+    return a[1] < b[1];
 }
 int solution(vector<vector<int>> routes) {
     int answer = 0;
     int now = 30001; 
+    //시작점 정렬은 차가 나가는 지점을 한번 더 확인해 주어야하는데 끝점 정렬로 하면 조건이 하나 줄어드네요
     sort(routes.begin(),routes.end(),cmp);
     
     for(auto it : routes){
